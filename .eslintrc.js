a439cb9fecfbc3018875e9ff51751c7427315de8
David Ethan Hwang <hdavidethan@gmail.com>
2023-05-29 16:41:33 -0700

    Fix workspace configuration

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["isaacscript", "import"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+    "plugin:@next/next/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  rules: {
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+    "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+
+    "isaacscript/complete-sentences-jsdoc": "warn",
+    "isaacscript/format-jsdoc-comments": "warn",
+  },
+};
+
+module.exports = config;
