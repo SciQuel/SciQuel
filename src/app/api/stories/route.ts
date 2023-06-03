6382bcd5b03611d8e5d649b644b7350029e548cd
David Ethan Hwang <hdavidethan@gmail.com>
2023-06-03 10:28:14 -0700

    Connect MainCard to backend

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,7 +1,28 @@
 import prisma from "@/lib/prisma";
+import { type ContributionType, type Story } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+export type GetStoriesResult = (Story & {
+  storyContributions: {
+    user: {
+      firstName: string;
+      lastName: string;
+    };
+    contributionType: ContributionType;
+  }[];
+})[];
+
 export async function GET() {
-  const stories = await prisma.story.findMany();
+  const stories = await prisma.story.findMany({
+    take: 4,
+    include: {
+      storyContributions: {
+        select: {
+          contributionType: true,
+          user: { select: { firstName: true, lastName: true } },
+        },
+      },
+    },
+  });
   return NextResponse.json(stories ?? []);
 }
--- src/components/WhatsNewSection/index.tsx
+++ src/components/WhatsNewSection/index.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { type GetStoriesResult } from "@/app/api/stories/route";
+import { type Story } from "@prisma/client";
+import axios from "axios";
+import { DateTime } from "luxon";
+import useSWR, { type Fetcher } from "swr";
 import ArticleList, { type Article } from "../ArticleList";
 import HomepageSection from "../HomepageSection";
 import MainCard from "../MainCard/MainCard";
 
+const fetcher: Fetcher<GetStoriesResult, string> = async (url) => {
+  return (await axios.get<GetStoriesResult>(url)).data.map((story) => ({
+    ...story,
+    createdAt: new Date(story.createdAt),
+    publishedAt: new Date(story.publishedAt),
+    updatedAt: new Date(story.updatedAt),
+  }));
+};
+
 export default function WhatsNewSection() {
+  const { data } = useSWR("/api/stories", fetcher);
+  console.log(data);
+
+  const headlineArticle = data?.[0];
+  const readMoreArticles = data?.slice(1, 4);
   const articles = [
     {
       topic: "COMPUTER_SCIENCE",
@@ -37,18 +58,29 @@ export default function WhatsNewSection() {
   return (
     <HomepageSection heading="Read what's new">
       <div className="pb-5">
-        <MainCard
-          thumbnailUrl="/assets/images/bobtail.png"
-          title={"Lights. Camera. Action!"}
-          subtitle={
-            "How does the Hawaiian bobtail squid singly live with Vibrio fischeri? A look at the Special Relationship between an uncommon bacterium and a pocket-sized squid."
-          }
-          author={"Edward Chen"}
-          date={"May 27, 2021"}
-          mediaType={"ARTICLE"}
-          tag="MEDICINE"
-          href={"/"}
-        />
+        {headlineArticle && (
+          <MainCard
+            thumbnailUrl={headlineArticle.thumbnailUrl}
+            title={headlineArticle.title}
+            subtitle={headlineArticle.summary}
+            author={(() => {
+              const author = headlineArticle.storyContributions.find(
+                (value) => value.contributionType === "AUTHOR",
+              );
+              return author
+                ? `${author.user.firstName} ${author.user.lastName}`
+                : "";
+            })()}
+            date={DateTime.fromJSDate(
+              headlineArticle.publishedAt,
+            ).toLocaleString(DateTime.DATE_FULL)}
+            mediaType={headlineArticle.storyType}
+            tag={headlineArticle.tags[0]}
+            href={`/${headlineArticle.publishedAt.getFullYear()}/${headlineArticle.publishedAt.getMonth()}/${headlineArticle.publishedAt.getDay()}/${
+              headlineArticle.slug
+            }`}
+          />
+        )}
       </div>
       <ArticleList articles={articles} />
     </HomepageSection>
