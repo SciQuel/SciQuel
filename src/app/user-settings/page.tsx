4e071756b2d97b2fa11d5ffed1675ae8a07edd0e
Yong Liang <yoliang@ucsd.edu>
2023-06-11 18:11:46 -0700

    implement quiz card

--- src/app/user-settings/page.tsx
+++ src/app/user-settings/page.tsx
@@ -1,10 +1,67 @@
 import GreetingCard from "@/components/UserSettings/GreetingCard";
+import QuizCard from "@/components/UserSettings/QuizCard";
+import { StoryTopic } from "@prisma/client";
+
+const mock_quiz_history = [
+  {
+    title: "Lights, Camera, Action!",
+    topic: StoryTopic.PHYSICS,
+    date: "May 15, 2022",
+    type: "Pre-Quiz",
+    total: 3,
+    score: 1,
+  },
+  {
+    title: "Lights, Camera, Action!",
+    topic: StoryTopic.PHYSICS,
+    date: "May 15, 2022",
+    type: "Post-Quiz",
+    total: 3,
+    score: 3,
+  },
+  {
+    title: "Lights, Camera, Action!",
+    topic: StoryTopic.PHYSICS,
+    date: "May 15, 2022",
+    type: "Pre-Quiz",
+    total: 3,
+    score: 1,
+  },
+  {
+    title: "Lights, Camera, Action!",
+    topic: StoryTopic.PHYSICS,
+    date: "May 15, 2022",
+    type: "Post-Quiz",
+    total: 3,
+    score: 3,
+  },
+  {
+    title: "Lights, Camera, Action!",
+    topic: StoryTopic.PHYSICS,
+    date: "May 15, 2022",
+    type: "Pre-Quiz",
+    total: 3,
+    score: 1,
+  },
+  {
+    title: "Lights, Camera, Action!",
+    topic: StoryTopic.PHYSICS,
+    date: "May 15, 2022",
+    type: "Post-Quiz",
+    total: 3,
+    score: 3,
+  },
+];
 
 export default function UserSetting() {
   return (
     <div className="relative flex grow flex-col pt-6 md:pl-56">
       <GreetingCard />
-      <div className="mt-6 flex min-h-[180px] w-full rounded-md border bg-white"></div>
+      <div className="mt-6 flex min-h-[180px] w-full flex-nowrap gap-4 overflow-auto ">
+        {mock_quiz_history.map((quizItem, idx) => (
+          <QuizCard idx={idx} quizItem={quizItem} />
+        ))}
+      </div>
 
       <div className="mt-6 flex grow flex-wrap gap-6 border-4">
         <div className="min-h-[300px] basis-full rounded-md border bg-white lg:flex-1"></div>
---
+++ src/components/UserSettings/QuizCard.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import TopicTag from "@/components/TopicTag";
+import { type StoryTopic } from "@prisma/client";
+import * as d3 from "d3";
+import { useEffect } from "react";
+
+interface QuizItem {
+  title: string;
+  topic: StoryTopic;
+  date: string;
+  type: string;
+  total: number;
+  score: number;
+}
+
+function createScoreDonut(quiz_obj: QuizItem, id_str: string) {
+  const selector = "#" + id_str;
+  const height = 120;
+  const width = 115;
+  const score = `${quiz_obj.score}/${quiz_obj.total}`;
+  const endAngle = 2 * Math.PI * (quiz_obj.score / quiz_obj.total);
+  const svg = d3
+    .select(selector)
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  const arc = d3
+    .arc()
+    .outerRadius(55)
+    .innerRadius(40)
+    .cornerRadius(15)
+    .startAngle(0)
+    .endAngle(endAngle);
+
+  const bg_arc = d3
+    .arc()
+    .outerRadius(55)
+    .innerRadius(40)
+    .startAngle(0)
+    .endAngle(2 * Math.PI);
+
+  svg.append("path").attr("d", bg_arc).attr("fill", "#D9D9D9");
+
+  svg
+    .append("path")
+    .attr("d", arc)
+    .attr("fill", () => {
+      if (quiz_obj.score / quiz_obj.total < 0.5) {
+        return "#E79595";
+      } else if (quiz_obj.score / quiz_obj.total < 0.75) return "#D66300";
+      return "#A3C9A8";
+    });
+  svg.append("text").text(score).attr("x", -15).attr("y", 6);
+}
+
+export default function QuizCard({
+  idx,
+  quizItem,
+}: {
+  idx: number;
+  quizItem: QuizItem;
+}) {
+  console.log(idx);
+  useEffect(() => {
+    createScoreDonut(quizItem, `quiz-history-item-${idx}`);
+  }, []);
+  return (
+    <div
+      className="flex w-80 flex-none items-center justify-center gap-4 rounded-md border bg-white"
+      key={idx}
+    >
+      <div
+        className="text-xl font-semibold"
+        id={`quiz-history-item-${idx}`}
+      ></div>
+      <div className="flex w-36 flex-col">
+        <div className="line-clamp-2 text-lg font-semibold">
+          {quizItem.title}
+        </div>
+        <div className="text-xs font-thin text-gray-400">{quizItem.type}</div>
+        <div className="my-2 w-14">
+          <TopicTag name={quizItem.topic} />
+        </div>
+        <div className="text-xs font-thin text-gray-400">{quizItem.date}</div>
+      </div>
+    </div>
+  );
+}
