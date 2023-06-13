5d973a70230f6d0bc6963a7b9ff8c584c7a30b42
David Ethan Hwang <hdavidethan@gmail.com>
2023-06-13 16:51:07 -0400

    Add story creation tool

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,6 +1,13 @@
 import prisma from "@/lib/prisma";
-import { type ContributionType, type Story } from "@prisma/client";
-import { NextResponse } from "next/server";
+import {
+  ContributionType,
+  StoryTopic,
+  StoryType,
+  type Story,
+} from "@prisma/client";
+import { NextResponse, type NextRequest } from "next/server";
+import slug from "slug";
+import { postStorySchema } from "./schema";
 
 export type GetStoriesResult = (Story & {
   storyContributions: {
@@ -28,4 +35,53 @@ export async function GET() {
     },
   });
   return NextResponse.json(stories ?? []);
+}
+
+export async function POST(request: NextRequest) {
+  const timestamp = new Date();
+  const parsedBody = postStorySchema.safeParse(await request.json());
+  if (!parsedBody.success) {
+    return NextResponse.json({ error: "Bad Request" }, { status: 400 });
+  }
+
+  const { title, content } = parsedBody.data;
+
+  const user = await prisma.user.findFirst({});
+  if (!user) {
+    return NextResponse.json({ error: "No user available" }, { status: 500 });
+  }
+
+  const storyContribution = await prisma.storyContribution.create({
+    data: {
+      user: { connect: { id: user.id } },
+      story: {
+        create: {
+          storyType: StoryType.ARTICLE,
+          title,
+          titleColor: "#ffffff",
+          slug: slug(title),
+          summary: `summary of ${title}`,
+          summaryColor: "#ffffff",
+          tags: [StoryTopic.ASTRONOMY],
+          published: true,
+          staffPick: Math.random() > 0.5,
+          thumbnailUrl: "/assets/images/bobtail.png",
+          createdAt: timestamp,
+          publishedAt: timestamp,
+          updatedAt: timestamp,
+        },
+      },
+      contributionType: ContributionType.AUTHOR,
+    },
+  });
+
+  await prisma.storyContent.create({
+    data: {
+      story: { connect: { id: storyContribution.storyId } },
+      content,
+      createdAt: timestamp,
+    },
+  });
+
+  return NextResponse.json({}, { status: 201 });
 }
