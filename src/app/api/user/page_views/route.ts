0436b5ac3ab8727ac8592c2059e58e15d6688519
Yesheng Liu <yesheng.liu@outlook.com>
2023-06-20 01:23:02 -0700

    Fixed error handling for pageview endpoint

--- src/app/api/user/page_views/route.ts
+++ src/app/api/user/page_views/route.ts
@@ -12,15 +12,16 @@ const bodySchema = z.object({
 });
 
 export async function POST(req: NextRequest) {
-  const result = bodySchema.safeParse(await req.json());
-  if (!result.success) {
-    return NextResponse.json(result.error, { status: 400 });
-  }
-
-  const { story_id } = result.data;
-
-  // validate story_id
+  let result;
   try {
+    result = bodySchema.safeParse(await req.json());
+    if (!result.success) {
+      return NextResponse.json(result.error, { status: 400 });
+    }
+
+    const { story_id } = result.data;
+
+    // validate story_id
     const story = await prisma.story.findUnique({
       where: {
         id: story_id,
@@ -30,26 +31,35 @@ export async function POST(req: NextRequest) {
     if (story === null) {
       return NextResponse.json({ error: "Story not found" }, { status: 404 });
     }
+
+    //TODO: retrieve user_id from session/cookie
+    const user_id = "647ad6fda9efff3abe83044f";
+
+    const res = await prisma.pageView.create({
+      data: {
+        userId: user_id,
+        storyId: story_id,
+      },
+    });
+
+    if (res === null) {
+      return NextResponse.json(
+        { error: "Failed to create db entry" },
+        { status: 500 },
+      );
+    }
+
+    return NextResponse.json(res, { status: 200 });
   } catch (e) {
-    return NextResponse.json({ error: e }, { status: 400 });
+    // req.json() throws an error
+    if (!result) {
+      return NextResponse.json(
+        { error: "Invalid request body" },
+        { status: 400 },
+      );
+    }
+
+    // all other errors
+    return NextResponse.json({ error: e }, { status: 500 });
   }
-
-  //TODO: retrieve user_id from session/cookie
-  const user_id = "647ad6fda9efff3abe83044f";
-
-  const res = await prisma.pageView.create({
-    data: {
-      userId: user_id,
-      storyId: story_id,
-    },
-  });
-
-  if (res === null) {
-    return NextResponse.json(
-      { error: "Failed to create db entry" },
-      { status: 500 },
-    );
-  }
-
-  return NextResponse.json(res, { status: 200 });
 }
