2d53bc3cd294154d03cdd4e9a22b17e17ac06bd6
David Ethan Hwang <hdavidethan@gmail.com>
2023-06-22 15:07:50 -0400

    Add FormInput component

---
+++ src/components/Form/FormInput/index.tsx
@@ -0,0 +1,42 @@
+import { type ChangeEventHandler, type HTMLProps } from "react";
+
+interface Props {
+  title: string;
+  required?: boolean;
+  indicateRequired?: boolean;
+  type?: "text" | "password";
+  value?: HTMLProps<HTMLInputElement>["value"];
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function FormInput({
+  title,
+  required = false,
+  indicateRequired = true,
+  type = "text",
+  value,
+  onChange,
+}: Props) {
+  return (
+    <div className="relative mt-6 w-full">
+      <input
+        className={`peer w-full rounded-md px-2 py-1 placeholder-transparent outline
+        outline-1 outline-gray-200 hover:outline-sciquelTeal
+        focus:outline-2 focus:outline-sciquelTeal focus:ring-0`}
+        placeholder={title}
+        type={type}
+        value={value}
+        onChange={onChange}
+      />
+      <label
+        className={`duration-400 pointer-events-none absolute -top-5 left-0 w-full
+        text-sm transition-position peer-placeholder-shown:left-2
+        peer-placeholder-shown:top-1 peer-placeholder-shown:text-base
+        peer-placeholder-shown:text-gray-400 peer-focus:text-black`}
+      >
+        {title}
+        {required && indicateRequired ? " *" : null}
+      </label>
+    </div>
+  );
+}
