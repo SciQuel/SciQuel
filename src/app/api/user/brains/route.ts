c68476731377a4e5592078cb5c62364761acd136
Yesheng Liu <yeshengliu@users.noreply.github.com>
2023-06-26 12:57:11 -0700

    Add prisma schema for brains (#22)
    * Add prisma schema for brains
    
    * Add endpoint for brains
    
    * Update doc for brain endpoint
    
    ---------
    
    Co-authored-by: David Ethan Hwang <hdavidethan@gmail.com>

---
+++ src/app/api/user/brains/route.ts
@@ -0,0 +1,91 @@
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { NextResponse, type NextRequest } from "next/server";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  story_id: z
+    .string({
+      required_error: "story_id is required",
+      invalid_type_error: "story_id must be a ObjectId",
+    })
+    .regex(/^[0-9a-f]{24}$/, { message: "story_id must be a valid ObjectId" }),
+
+  user_id: z
+    .string({
+      required_error: "user_id is required",
+      invalid_type_error: "user_id must be a ObjectId",
+    })
+    .regex(/^[0-9a-f]{24}$/, { message: "user_id must be a valid ObjectId" }),
+});
+
+export async function POST(req: NextRequest) {
+  let result;
+  try {
+    result = bodySchema.safeParse(await req.json());
+    if (!result.success) {
+      return NextResponse.json(result.error, { status: 400 });
+    }
+
+    const { story_id, user_id } = result.data;
+
+    // validate story_id
+    const story = await prisma.story.findUnique({
+      where: {
+        id: story_id,
+      },
+    });
+
+    if (story === null) {
+      return NextResponse.json({ error: "Story not found" }, { status: 404 });
+    }
+
+    // validate user_id
+    const user = await prisma.user.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (user === null) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const res = await prisma.brain.create({
+      data: {
+        userId: user_id,
+        storyId: story_id,
+      },
+    });
+
+    if (res === null) {
+      return NextResponse.json(
+        { error: "Failed to create db entry" },
+        { status: 500 },
+      );
+    }
+
+    return NextResponse.json(res, { status: 200 });
+  } catch (e) {
+    // req.json() throws an error
+    if (!result) {
+      return NextResponse.json(
+        { error: "Invalid request body" },
+        { status: 400 },
+      );
+    }
+
+    // user try to create duplicate entries
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return NextResponse.json(
+          { error: "story_id and user_id combination already exists" },
+          { status: 400 },
+        );
+      }
+    }
+
+    // all other errors
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
+}
