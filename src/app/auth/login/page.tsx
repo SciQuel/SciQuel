b79917e47ab62fd0e3a664c293b3ad024330f60c
David Ethan Hwang <hdavidethan@gmail.com>
2023-07-08 15:18:21 -0400

    Add error messages to login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,30 @@
+import Alert from "@/components/Alert";
 import LoginForm from "@/components/Auth/LoginForm";
 
-export default function LoginPage() {
-  return <LoginForm />;
+export default function LoginPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: unknown };
+}) {
+  const errorMap: Record<string, JSX.Element> = {
+    EmailSignin: (
+      <Alert type="danger">
+        There was a problem while attempting to verify your account.
+      </Alert>
+    ),
+    CredentialsSignin: (
+      <Alert type="danger">
+        We could not find an account matching that email and password.
+      </Alert>
+    ),
+  };
+
+  const error = searchParams.error;
+
+  return (
+    <>
+      <LoginForm />
+      <div className="mt-6">{typeof error === "string" && errorMap[error]}</div>
+    </>
+  );
 }
