95438ae78ee8de01194ea33a3e32d10a149157ba
David Ethan Hwang <hdavidethan@gmail.com>
2023-07-12 03:02:34 -0400

    Remove console.log

--- src/components/MarkdownEditor/StatusBar.tsx
+++ src/components/MarkdownEditor/StatusBar.tsx
@@ -3,7 +3,6 @@ interface Props {
 }
 
 export default function StatusBar({ wordStats }: Props) {
-  console.log(wordStats);
   return (
     <div className="flex flex-row border-t bg-slate-100 px-2 py-1 text-xs">
       <div>Word Count: {wordStats["WordNode"] ?? 0}</div>
