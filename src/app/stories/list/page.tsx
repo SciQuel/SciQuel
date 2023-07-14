67a6a628e9dcd6b59be49e4cc35a8fa9dd296169
Yesheng Liu <yesheng.liu@outlook.com>
2023-07-13 17:40:39 -0700

    Modify page to adapt topic and staff pick as query param

--- src/app/stories/list/page.tsx
+++ src/app/stories/list/page.tsx
@@ -1,31 +1,53 @@
+"use client";
+
 import { type GetStoriesResult } from "@/app/api/stories/route";
 import ArticleList from "@/components/ArticleList";
 import HomepageSection from "@/components/HomepageSection";
 import env from "@/lib/env";
+import { useSearchParams } from "next/navigation";
 
-interface Params {
-  params: {
-    topic: string;
+export default async function StoriesListPage() {
+  const searchParams = useSearchParams();
+  const topic = searchParams.get("topic");
+  const staff_pick = searchParams.get("staff_pick");
+
+  const params = {
+    ...(topic ? { topic } : {}),
+    ...(staff_pick && staff_pick === "true" ? { staff_pick: "true" } : {}),
   };
-}
 
-export default async function StoriesListPage({ params }: Params) {
-  const { topic } = params;
   const articles = await getStories(params);
+
+  // Header text shows ALL TOPICS by default or Topic if specified
+  let headerText = topic ? topic.toUpperCase() : "ALL TOPICS";
+
+  // Add Staff Pick to header text if user specified staff pick
+  if (staff_pick && staff_pick === "true") {
+    headerText += " | Staff Picks";
+  }
 
   return (
     <>
       <div className="mx-[10%] my-10 flex flex-col gap-12">
-        <HomepageSection heading={`${topic.toUpperCase()}`}>
-          <ArticleList articles={articles} preferHorizontal={true} />
+        <HomepageSection heading={headerText}>
+          {articles.length > 0 ? (
+            <ArticleList articles={articles} preferHorizontal={true} />
+          ) : (
+            <h2 className="text-3xl font-[550] text-sciquelHeading">
+              No Result
+            </h2>
+          )}
         </HomepageSection>
       </div>
     </>
   );
 }
 
-async function getStories({ topic }: Params["params"]) {
-  const route = `/stories?topic=${topic}`;
+async function getStories(params: Record<string, string>) {
+  const searchParams = new URLSearchParams(params);
+  const route = `/stories?${searchParams.toString()}`;
+  console.log(route);
+
   const res = await fetch(`${env.NEXT_PUBLIC_SITE_URL}/api${route}`, {
     next: { revalidate: 60 },
   });
