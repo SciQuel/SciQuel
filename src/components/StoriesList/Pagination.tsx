cfa45c2f63a258c98cfe28a781a338638660afb1
Yesheng Liu <yesheng.liu@outlook.com>
2023-07-19 20:06:19 -0700

    Implement ... in pagination

--- src/components/StoriesList/Pagination.tsx
+++ src/components/StoriesList/Pagination.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname, useSearchParams } from "next/navigation";
+import PaginationNumber from "./PaginationNumber";
 
 interface Props {
   total_pages: number;
@@ -11,56 +12,93 @@ export default function Pagination({ total_pages }: Props) {
   if (!params.page) {
     params.page = "1";
   }
-  const page_number = parseInt(params.page);
-  const pages = Array.from({ length: total_pages }, (_, i) => i + 1);
+  const curr_page = parseInt(params.page);
+
+  const pageList = createPageList(params, curr_page, total_pages);
 
   return (
     <>
       <ul className="inline-flex justify-center gap-4 align-middle text-lg text-sciquelMuted">
         <li>Page</li>
-        {page_number !== 1 && (
-          <li>
-            <a
-              href={`${usePathname()}?${new URLSearchParams({
-                ...params,
-                page: String(page_number - 1),
-              }).toString()}`}
-            >
-              {"<"} Previous
-            </a>
-          </li>
-        )}
-        {pages.map((page) => (
-          <li key={`pagination-item-${page}`}>
-            <a
-              className={
-                (page === page_number
-                  ? "bg-[#69A297] text-black"
-                  : "transition-all duration-300 hover:bg-neutral-100") +
-                " rounded-full px-3.5 py-1.5"
-              }
-              href={`${usePathname()}?${new URLSearchParams({
-                ...params,
-                page: String(page),
-              }).toString()}`}
-            >
-              {page}
-            </a>
-          </li>
-        ))}
-        {page_number !== total_pages && (
-          <li>
-            <a
-              href={`${usePathname()}?${new URLSearchParams({
-                ...params,
-                page: String(page_number + 1),
-              }).toString()}`}
-            >
-              Next {">"}
-            </a>
-          </li>
-        )}
+        {pageList}
       </ul>
     </>
   );
+}
+
+function createPageList(
+  params: {
+    [k: string]: string;
+  },
+  curr_page: number,
+  total_pages: number,
+) {
+  const pageList = [];
+  const offset = 2; // offset of pages before and after the current page
+
+  if (curr_page !== 1) {
+    pageList.push(
+      <li>
+        <a
+          href={`${usePathname()}?${new URLSearchParams({
+            ...params,
+            page: String(curr_page - 1),
+          }).toString()}`}
+        >
+          {"< Previous"}
+        </a>
+      </li>,
+    );
+  }
+
+  if (curr_page - offset > 1) {
+    pageList.push(
+      <PaginationNumber params={params} page={1} curr_page={curr_page} />,
+    );
+  }
+
+  if (curr_page - offset - 1 > 1) {
+    pageList.push(<li>...</li>);
+  }
+
+  for (let page = curr_page - offset; page <= curr_page + offset; page++) {
+    if (page < 1 || page > total_pages) {
+      continue;
+    }
+
+    pageList.push(
+      <PaginationNumber params={params} page={page} curr_page={curr_page} />,
+    );
+  }
+
+  if (curr_page + offset + 1 < total_pages) {
+    pageList.push(<li>...</li>);
+  }
+
+  if (curr_page + offset < total_pages) {
+    pageList.push(
+      <PaginationNumber
+        params={params}
+        page={total_pages}
+        curr_page={curr_page}
+      />,
+    );
+  }
+
+  if (curr_page !== total_pages) {
+    pageList.push(
+      <li>
+        <a
+          href={`${usePathname()}?${new URLSearchParams({
+            ...params,
+            page: String(curr_page + 1),
+          }).toString()}`}
+        >
+          {"Next >"}
+        </a>
+      </li>,
+    );
+  }
+
+  return pageList;
 }
