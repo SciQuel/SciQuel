5a50555759dcceb4d5a0fa86a30791e96e9b763d
Yong Liang <yoliang@ucsd.edu>
2023-07-21 08:02:27 +0800

    first commit

--- src/app/user-settings/dashboard/@greeting/page.tsx
+++ src/app/user-settings/dashboard/@greeting/page.tsx
@@ -1,13 +1,19 @@
-"use client";
-
 import GreetingCard from "@/components/UserSettings/GreetingCard";
+import env from "@/lib/env";
+import { type User } from "@prisma/client";
 import { useSession } from "next-auth/react";
 
-export default function Greeting() {
+export default async function Greeting() {
   const { data: session } = useSession();
-  let userName = "James";
-  if (session && session.user) {
-    userName = String(session.user.name);
+  if (!session || !session.user || !session?.user?.email) return null;
+  const userEmail = session.user.email;
+  const res = await fetch(
+    `${env.NEXT_PUBLIC_SITE_URL}/api/settings/user?email=${userEmail}`,
+  );
+  if (!res.ok) {
+    return null;
   }
-  return <GreetingCard name={userName} />;
+  const user = (await res.json()) as User;
+
+  return <GreetingCard user={user} />;
 }
