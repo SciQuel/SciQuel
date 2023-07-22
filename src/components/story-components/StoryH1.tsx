dc9c2210358319299136f0f3f6e061cf1744c2bc
David Ethan Hwang <hdavidethan@gmail.com>
2023-07-21 23:42:35 -0400

    Revert story component styling changes

--- src/components/story-components/StoryH1.tsx
+++ src/components/story-components/StoryH1.tsx
@@ -1,18 +1,9 @@
-import clsx from "clsx";
 import { type PropsWithChildren } from "react";
 
 export default function StoryH1({ children }: PropsWithChildren<unknown>) {
   return (
-    <div className="flex justify-center">
-      <h1
-        className={clsx(
-          "mt-5 text-4xl font-medium text-sciquelTeal",
-          "@lg:w-[28rem] @3xl:w-[35rem] @5xl:w-[40rem] @7xl:w-[50rem]",
-          "mx-10 @lg:mx-0",
-        )}
-      >
-        {children}
-      </h1>
-    </div>
+    <h1 className="mx-[30rem] text-4xl font-medium text-sciquelTeal">
+      {children}
+    </h1>
   );
 }
--- src/components/story-components/StoryParagraph.tsx
+++ src/components/story-components/StoryParagraph.tsx
@@ -1,21 +1,11 @@
-import clsx from "clsx";
 import { type PropsWithChildren } from "react";
 
 export default function StoryParagraph({
   children,
 }: PropsWithChildren<unknown>) {
   return (
-    <div className="flex justify-center">
-      <p
-        className={clsx(
-          "font-sourceSerif4 text-lg font-[370] leading-8",
-          "@lg:w-[28rem] @3xl:w-[35rem] @5xl:w-[40rem] @7xl:w-[50rem]",
-          "mx-10 @lg:mx-0",
-          "story-paragraph",
-        )}
-      >
-        {children}
-      </p>
-    </div>
+    <p className="mx-[30rem] font-sourceSerif4 text-lg font-[370] leading-8">
+      {children}
+    </p>
   );
 }
