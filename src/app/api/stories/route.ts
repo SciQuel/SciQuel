a5b94f4bb1163cc3a13744766ef0f93801e80a25
Yesheng Liu <yesheng.liu@outlook.com>
2023-07-24 12:31:35 -0700

    Refactor /api/stories to call /api/stories/search directly

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,7 +1,6 @@
 import prisma from "@/lib/prisma";
 import {
   ContributionType,
-  Prisma,
   StoryTopic,
   StoryType,
   type Story,
@@ -9,6 +8,7 @@ import {
 import { NextResponse, type NextRequest } from "next/server";
 import slug from "slug";
 import { postStorySchema } from "./schema";
+import { GET as getSearchStories } from "./search/route";
 
 export type Stories = (Story & {
   storyContributions: {
@@ -27,103 +27,7 @@ export type GetStoriesResult = {
 };
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const page = searchParams.get("page");
-  const pageSize = searchParams.get("page_size");
-  const staffPick = searchParams.get("staff_pick");
-  const topic = searchParams.get("topic");
-
-  let numPagesToSkip = 0;
-  let numStoriesPerPage = 10; // default page size
-
-  if (pageSize !== null) {
-    // user specified page size
-    const parsedPageSize =
-      typeof pageSize === "string" ? parseInt(pageSize) : null;
-
-    if (
-      parsedPageSize === null ||
-      isNaN(parsedPageSize) ||
-      parsedPageSize < 1
-    ) {
-      // user requests a invalid page size
-      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
-    }
-
-    numStoriesPerPage = parsedPageSize;
-  }
-
-  if (page === null) {
-    // return first page if user does not specify a page
-    numPagesToSkip = 0;
-  } else {
-    // user specified page
-    const parsedPage = typeof page === "string" ? parseInt(page) : null;
-
-    if (parsedPage === null || isNaN(parsedPage) || parsedPage < 1) {
-      // user requests a invalid page
-      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
-    }
-
-    numPagesToSkip = parsedPage - 1;
-  }
-
-  let parsedTopic: StoryTopic | null = null;
-  if (topic !== null) {
-    parsedTopic =
-      typeof topic === "string" && topic.length > 0
-        ? (topic.toUpperCase() as StoryTopic)
-        : null;
-
-    if (parsedTopic === null) {
-      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
-    }
-  }
-
-  try {
-    const query = {
-      where: {
-        staffPick: staffPick === "true" ? true : undefined,
-        ...(parsedTopic ? { tags: { has: parsedTopic } } : {}),
-      },
-    };
-
-    const stories = await prisma.story.findMany({
-      skip: numPagesToSkip * numStoriesPerPage,
-      take: numStoriesPerPage,
-      include: {
-        storyContributions: {
-          select: {
-            contributionType: true,
-            user: { select: { firstName: true, lastName: true } },
-          },
-        },
-      },
-      where: query.where,
-      orderBy: {
-        updatedAt: "desc",
-      },
-    });
-
-    const numStories = await prisma.story.count({
-      where: query.where,
-    });
-
-    return NextResponse.json(
-      {
-        stories,
-        page_number: numPagesToSkip + 1,
-        total_pages: Math.ceil(numStories / numStoriesPerPage),
-      } ?? { stories: [] },
-    );
-  } catch (e) {
-    if (e instanceof Prisma.PrismaClientValidationError) {
-      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
-    }
-
-    // all other errors
-    return NextResponse.json({ error: e }, { status: 500 });
-  }
+  return getSearchStories(req);
 }
 
 export async function POST(request: NextRequest) {
