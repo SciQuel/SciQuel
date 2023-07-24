32852ebf9ed622e6420a99c0ec4442c2ec5c4310
Yesheng Liu <yesheng.liu@outlook.com>
2023-07-24 16:14:29 -0700

    Merge /api/stories/search into /api/stories

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,14 +1,24 @@
 import prisma from "@/lib/prisma";
 import {
   ContributionType,
+  Prisma,
   StoryTopic,
   StoryType,
   type Story,
 } from "@prisma/client";
 import { NextResponse, type NextRequest } from "next/server";
 import slug from "slug";
-import { postStorySchema } from "./schema";
-import { GET as getSearchStories } from "./search/route";
+import { getStorySchema, postStorySchema } from "./schema";
+
+interface GetStoryQuery {
+  where: {
+    OR?: object[];
+    staffPick: boolean | undefined;
+    tags?: object;
+    storyType: StoryType | undefined;
+    createdAt: object;
+  };
+}
 
 export type Stories = (Story & {
   storyContributions: {
@@ -27,7 +37,91 @@ export type GetStoriesResult = {
 };
 
 export async function GET(req: Request) {
-  return getSearchStories(req);
+  const { searchParams } = new URL(req.url);
+  const parsedParams = getStorySchema.safeParse(
+    Object.fromEntries(searchParams),
+  );
+  if (!parsedParams.success) {
+    return NextResponse.json(parsedParams.error, { status: 400 });
+  }
+
+  const {
+    page,
+    page_size,
+    keyword,
+    staff_pick,
+    topic,
+    type,
+    date_from,
+    date_to,
+    sort_by,
+  } = parsedParams.data;
+
+  const numPagesToSkip = (page && page - 1) || 0;
+  const numStoriesPerPage = page_size || 10; // default page size
+
+  // to include stories created on that date
+  date_to?.setDate(date_to.getDate() + 1);
+
+  try {
+    const query: GetStoryQuery = {
+      where: {
+        ...(keyword
+          ? {
+              OR: [
+                { title: { contains: keyword, mode: "insensitive" } },
+                { summary: { contains: keyword, mode: "insensitive" } },
+              ],
+            }
+          : {}),
+        staffPick: staff_pick,
+        ...(topic ? { tags: { has: topic } } : {}),
+        storyType: type,
+        createdAt: {
+          gte: date_from,
+          lt: date_to,
+        },
+      },
+    };
+
+    const stories = await prisma.story.findMany({
+      skip: numPagesToSkip * numStoriesPerPage,
+      take: numStoriesPerPage,
+      include: {
+        storyContributions: {
+          select: {
+            contributionType: true,
+            user: { select: { firstName: true, lastName: true } },
+          },
+        },
+      },
+      where: query.where,
+      orderBy: {
+        ...(sort_by === "newest" ? { updatedAt: "desc" } : {}),
+        ...(sort_by === "oldest" ? { updatedAt: "asc" } : {}),
+      },
+    });
+
+    const numStories = await prisma.story.count({
+      where: query.where,
+    });
+
+    return NextResponse.json(
+      {
+        stories,
+        page_number: numPagesToSkip + 1,
+        total_pages: Math.ceil(numStories / numStoriesPerPage),
+      } ?? { stories: [] },
+    );
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientValidationError) {
+      console.log(e.message);
+      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
+    }
+
+    // all other errors
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
 }
 
 export async function POST(request: NextRequest) {
