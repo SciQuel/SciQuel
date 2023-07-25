a4874e9ffd42558654ecb7a53f2e8163000834f9
David Ethan Hwang <hdavidethan@gmail.com>
2023-07-25 09:41:16 -0400

    Create Avatar component

--- src/components/UserSettings/GreetingCard.tsx
+++ src/components/UserSettings/GreetingCard.tsx
@@ -1,6 +1,6 @@
+import Avatar from "@/components/Avatar";
 import TopicTag from "@/components/TopicTag";
 import { StoryTopic } from "@prisma/client";
-import Avatar from "/public/user-settings/ProfilePicture.png";
 import ArticleImage from "/public/user-settings/top_background_img.png";
 import Image from "next/image";
 
@@ -17,14 +17,8 @@ export default function GreetingCard(props: { name: string }) {
   return (
     <section className="flex min-h-[320px] flex-wrap overflow-hidden rounded-md border bg-white">
       <div className="relative flex min-h-[280px] flex-1 flex-col items-center p-6 sm:flex-row sm:items-start lg:basis-7/12">
-        <div className="flex h-24 w-24 flex-none items-center rounded-full bg-gradient-to-b from-[#A1C9C1] to-[#58ABF8] sm:h-36 sm:w-36">
-          <Image
-            src={Avatar}
-            alt="avatar"
-            className="mx-auto w-20 rounded-full sm:w-32"
-            width={100}
-            height={100}
-          />
+        <div className="flex h-[6.75rem] w-[6.75rem] items-center justify-center rounded-full bg-gradient-to-b from-[#A1C9C1] to-[#58ABF8]">
+          <Avatar label="A" size="2xl" />
         </div>
         <div className="mt-2 sm:ml-6">
           <p className="line-clamp-2 text-2xl font-semibold sm:text-3xl">
