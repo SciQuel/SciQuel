b34be315b1df402599c0db31bb1b72cd501e3b4f
Yesheng Liu <yesheng.liu@outlook.com>
2023-07-30 12:29:45 -0700

    Implement GET for /api/stories/page_views

---
+++ src/app/api/stories/page_views/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const getSchema = z.object({
+  story_id: z
+  .string({
+    required_error: "story_id is required",
+    invalid_type_error: "story_id must be a ObjectId",
+  })
+  .regex(/^[0-9a-f]{24}$/, { message: "story_id must be a valid ObjectId" })
+});
