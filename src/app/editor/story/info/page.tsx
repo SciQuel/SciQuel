e86856ff7820654163c7e34edb7de4cb65eb0f77
David Ethan Hwang <hdavidethan@gmail.com>
2023-08-09 16:19:14 +0800

    Redirect to dashboard on invalid story id

--- src/app/editor/story/info/page.tsx
+++ src/app/editor/story/info/page.tsx
@@ -1,5 +1,6 @@
 import StoryInfoForm from "@/components/EditorDashboard/StoryInfoForm";
 import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 interface SearchParams {
   id?: string;
@@ -11,6 +12,9 @@ export default async function StoryInfoEditorPage({
   searchParams: SearchParams;
 }) {
   const story = id ? await prisma.story.findUnique({ where: { id } }) : null;
+  if (id && !story) {
+    return redirect("/editor/dashboard");
+  }
   return (
     <div className="mx-32 mt-5 flex flex-col gap-5">
       <h3 className="text-3xl font-semibold text-sciquelTeal">Story Info</h3>
