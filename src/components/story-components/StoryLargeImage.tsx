60a3031bce578ec81095d734c4afcd98fc5d988c
Jason Xu <jasohxu@gmail.com>
2023-09-02 10:38:15 -0400

    Update StoryLargeImage.tsx
    Added zooming and scrolling feature to image when clicked

--- src/components/story-components/StoryLargeImage.tsx
+++ src/components/story-components/StoryLargeImage.tsx
@@ -1,27 +1,90 @@
-import { type PropsWithChildren } from "react";
+"use client"
 
-interface Props {
-  src: string;
-  alt?: string;
+import React, { useState, useRef, useEffect, MouseEvent, PropsWithChildren } from "react";
+
+function ExpandedImage({ src, children, onOutsideClick }: PropsWithChildren<{ src: string; onOutsideClick: () => void; }>) {
+  const [zoomed, setZoomed] = useState(false);
+  const [isCaptionExpanded, setIsCaptionExpanded] = useState(false);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
+  const handleImageClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    const img = imageRef.current;
+    if (img) {
+      const rect = img.getBoundingClientRect();
+      const maxZoom = Math.min(img.naturalWidth / rect.width, img.naturalHeight / rect.height);
+      const { clientX: x, clientY: y } = e;
+      img.style.transformOrigin = `${((x - rect.left) / rect.width) * 100}% ${((y - rect.top) / rect.height) * 100}%`;
+
+      img.style.transform = zoomed ? "scale(1)" : `scale(${maxZoom})`;
+      setZoomed(!zoomed);
+    }
+  };
+
+  const toggleCaption = (e: MouseEvent) => {
+    e.stopPropagation();
+    setIsCaptionExpanded(!isCaptionExpanded);
+  };
+
+  const renderCaption = () => {
+    if (isCaptionExpanded) return children;
+  
+    const approximateLength = 150;
+    
+    let firstLine = "";
+    let currentLength = 0;
+  
+    React.Children.forEach(children, child => {
+      if (typeof child === 'string' && currentLength < approximateLength) {
+        for (let char of child) {
+          if (currentLength < approximateLength) {
+            firstLine += char;
+            currentLength++;
+          } else {
+            break;
+          }
+        }
+      }
+    });
+  
+    if (firstLine.length < React.Children.toArray(children).join("").length) {
+      firstLine += "...";
+    }
+  
+    return firstLine;
+  };
+
+
+  return (
+    <div className="fixed inset-0 z-[100] flex flex-col justify-center items-center bg-white p-4 overflow-auto" onClick={onOutsideClick}>
+      {!zoomed && <button className="z-[101] absolute top-4 right-4 w-8 h-8 bg-gray-600 text-white flex justify-center items-center rounded-full focus:outline-none" onClick={onOutsideClick}>X</button>}
+      <div className="flex justify-center">
+        <img ref={imageRef} src={src} className={`max-w-screen max-h-[85vh] object-contain relative z-[10] ${!zoomed ? 'cursor-zoom-in' : ' cursor-zoom-out'}`} onClick={handleImageClick} />
+      </div>
+      {!zoomed && <figcaption className="pb-4 mt-4 text-center cursor-pointer absolute bottom-0 w-full bg-white text-slate-800 z-[20]" onClick={toggleCaption}>{renderCaption()}</figcaption>}
+    </div>
+  );
 }
 
-export default function StoryLargeImage({
-  src,
-  alt,
-  children,
-}: PropsWithChildren<Props>) {
+export default function StoryLargeImage({ src, children }: PropsWithChildren<{ src: string }>) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className="flex justify-center">
       <figure className="mx-auto table gap-2 p-8 lg:w-min lg:p-0">
-        <img
-          src={src}
-          className="max-w-screen max-h-[900px] w-auto lg:max-w-[1000px]"
-          alt={alt}
-        />
+        <img src={src} className="max-w-screen max-h-[900px] w-auto lg:max-w-[1000px]" onClick={() => setIsExpanded(true)} />
         <figcaption className="table-caption w-full caption-bottom px-8 lg:px-0">
           {children}
         </figcaption>
       </figure>
+      {isExpanded && <ExpandedImage src={src} onOutsideClick={() => setIsExpanded(false)}>{children}</ExpandedImage>}
     </div>
   );
 }
