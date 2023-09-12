b1c6fa0fa2b145f5a846c7b4345936927ae9642e
kwu0103 <01wkl03@gmail.com>
2023-09-12 12:57:00 -0400

    Tmp update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,32 +3,24 @@ import ArticleList from "@/components/ArticleList";
 import Avatar from "@/components/Avatar";
 import FooterIcon from "@/components/Footer/FooterIcon";
 import HomepageSection from "@/components/HomepageSection";
+import ProfileButton from "@/components/ProfileButtons";
 import Pagination from "@/components/StoriesList/Pagination";
 import TopicTag from "@/components/TopicTag";
 import env from "@/lib/env";
 
 interface Params {
-  topic: string;
-  staff_pick: string;
-  trending: string;
+  searchParams: { [key: string]: string };
   page_number: string;
 }
-// export default async function ProfilePage() {
-//   const [whatsNewArticles] = await Promise.all([getWhatsNewArticles()]);
-//   return (
-
-//   );
-// }
 
 export default async function ProfilePage({
   searchParams,
-}: {
-  searchParams: Params;
-}) {
-  const { topic, staff_pick, page_number } = searchParams;
+  page_number,
+}: Params) {
+  const { id, category } = searchParams;
   const params = {
-    ...(topic ? { topic } : {}),
-    ...(staff_pick && staff_pick === "true" ? { staff_pick: "true" } : {}),
+    ...(id ? { id } : {}),
+    ...(category ? { category } : {}),
     page: page_number || "1",
   };
 
@@ -68,17 +60,7 @@ export default async function ProfilePage({
               <div className="w-1/6" />
               <div className="flex w-5/6 p-6 text-center">
                 <div className="h-full">
-                  <div className="flex flex-row gap-4 text-left text-2xl uppercase ">
-                    <button className="text-whit rounded border bg-[#A3C9A8] px-3 py-1 text-[#344E41] outline">
-                      Recent
-                    </button>
-                    <button className="text-whit rounded border bg-[#344E41] px-3 py-1 text-white outline">
-                      Staff Picks
-                    </button>
-                    <button className="text-whit rounded border bg-[#344E41] px-3 py-1 text-white outline">
-                      Trending
-                    </button>
-                  </div>
+                  <ProfileButton searchParams={searchParams} />
                   <div>
                     <div className="my-3 text-left">
                       <HomepageSection heading="Recent Articles" />
@@ -122,25 +104,4 @@ async function getStories(params: Record<string, string>) {
   }));
 
   return data;
-}
-
-async function getWhatsNewArticles() {
-  const res = await fetch(`${env.NEXT_PUBLIC_SITE_URL}/api/stories`, {
-    next: { revalidate: 60 },
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const stories = await res
-    .json()
-    .then((value: GetStoriesResult) => value.stories);
-
-  return stories.map((story) => ({
-    ...story,
-    createdAt: new Date(story.createdAt),
-    publishedAt: new Date(story.publishedAt),
-    updatedAt: new Date(story.updatedAt),
-  }));
 }
