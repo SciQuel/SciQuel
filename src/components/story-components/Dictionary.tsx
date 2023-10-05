7d6cf2de1b428bbe682a2b2b19fc00d9794c8bad
Hillary Hwang <hillaryhwang123@gmail.com>
2023-10-05 18:02:01 -0400

    basic dictionary functions, some temporary

--- src/components/story-components/Dictionary.tsx
+++ src/components/story-components/Dictionary.tsx
@@ -1,134 +1,125 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useContext } from "react";
 import closeButton from "../../../public/assets/images/close.png";
-
-interface DictionaryWord {
-  word: string;
-  definition: string;
-  inContext: string[];
-  pronunciation: string;
-}
-
-const testWords: DictionaryWord[] = [
-  {
-    word: "camouflage",
-    definition:
-      "Tum et sublimibus moenia aut tamen terras, alto arcus servare.",
-    pronunciation: "cam·ou·flage \n/ˈkaməˌflä(d)ZH/",
-    inContext: [
-      "Grave harum viridis iuvenis et guttae ingens bibulas pars, ubi.",
-      "Populator iam, ille, curvamine munus certaminis tenues: cum nec?",
-    ],
-  },
-  {
-    word: "enzymes",
-    definition:
-      "Tum et sublimibus moenia aut tamen terras, alto arcus servare.",
-    pronunciation: "en·zyme \n/ˈenˌzīm/",
-    inContext: [
-      "Grave harum viridis iuvenis et guttae ingens bibulas pars, ubi.",
-      "Populator iam, ille, curvamine munus certaminis tenues: cum nec?",
-    ],
-  },
-  {
-    word: "lipopolysaccharides",
-    definition:
-      "Tum et sublimibus moenia aut tamen terras, alto arcus servare.",
-    pronunciation:
-      "lip·o·pol·y·sac·cha·ride \n/ˌlipōˌpälēˈsakəˌrīd,ˌlīpōˌpälēˈsakəˌrīd/",
-    inContext: [
-      "Grave harum viridis iuvenis et guttae ingens bibulas pars, ubi.",
-      "Populator iam, ille, curvamine munus certaminis tenues: cum nec?",
-    ],
-  },
-];
+import {
+  DictionaryContext,
+  type SelectedDefinition,
+} from "./DictionaryContext";
 
 export default function Dictionary() {
-  const [isOpen, setIsOpen] = useState(true);
+  const fullDictionary = useContext(DictionaryContext);
 
-  const [selectedItem, setSelectedItem] = useState<null | DictionaryWord>(null);
-
-  return (
-    <div
-      className={`${
-        isOpen ? "border-4 " : "max-w-0 "
-      } fixed inset-y-0 right-0 flex h-screen w-80 flex-col justify-between self-end  border-sciquelTeal bg-sciquelCardBg pt-24`}
-    >
-      <div className="w-100 flex justify-end px-5 align-middle">
-        <button
-          type="button"
-          onClick={() => {
-            setIsOpen(false);
-          }}
-        >
-          <Image
-            className="opacity-75 hover:opacity-100"
-            alt="close dictionary"
-            src={closeButton}
-            width={20}
-            height={20}
-          />
-        </button>
-      </div>
-      <div className="flex-1">
-        {selectedItem ? (
-          <div className="px-4 py-2 font-sourceSerif4">
-            <div className="border-b-2 border-sciquelTeal">
-              <p className="text-sciquelCitation ">Term</p>
-              <p className="pb-2 pt-3 text-center font-bold">
-                {selectedItem.word}
-              </p>
-              <p className="whitespace-pre-line pb-4 text-center">
-                {selectedItem.pronunciation}
-              </p>
-            </div>
-            <p className="mt-2 text-sciquelCitation">Definition</p>
-            <p>{selectedItem.definition}</p>
-            <div className="my-2 w-2/5 border-b-2 border-sciquelTeal" />
-            <p className="text-sciquelCitation">In Context</p>
-            <p>{selectedItem.inContext}</p>
-            <div className="my-2 w-2/5 border-b-2 border-sciquelTeal" />
-            <p className="text-sciquelCitation">Synonyms & Antonyms</p>
-          </div>
-        ) : (
-          testWords.map((item, index) => {
-            return (
-              <div className="px-4 py-2 font-sourceSerif4">
-                <p className="text-sciquelCitation">Term</p>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setSelectedItem(item);
-                  }}
-                >
-                  {item.word}
-                </button>
-
-                <div className="mb-2 mt-1 w-2/5 border-b-2 border-sciquelTeal" />
-                <p className="text-sciquelCitation">Definition</p>
-                <p className="border-b-2 border-sciquelTeal py-2">
-                  {item.definition}
+  if (fullDictionary) {
+    return (
+      <div
+        className={`${
+          fullDictionary.open ? "border-4 " : "max-w-0 "
+        } fixed inset-y-0 right-0 z-10 flex h-screen w-80 flex-col justify-between self-end border-sciquelTeal bg-sciquelCardBg pt-20`}
+      >
+        <div className="w-100 flex justify-end px-5 align-middle">
+          <button
+            type="button"
+            onClick={() => {
+              fullDictionary.setOpen(false);
+            }}
+          >
+            <Image
+              className="opacity-75 hover:opacity-100"
+              alt="close dictionary"
+              src={closeButton}
+              width={20}
+              height={20}
+            />
+          </button>
+        </div>
+        <div className="flex-1 overflow-y-scroll">
+          {fullDictionary.word?.word ? (
+            <div className="px-4 py-2 font-sourceSerif4">
+              <div className="border-b-2 border-sciquelTeal">
+                <p className="text-sciquelCitation ">Term</p>
+                <p className="pb-2 pt-3 text-center font-bold">
+                  {fullDictionary.word.word}
+                </p>
+                <p className="whitespace-pre-line pb-4 text-center">
+                  {fullDictionary.word.pronunciation}
                 </p>
               </div>
-            );
-          })
+              <p className="mt-2 text-sciquelCitation">Definition</p>
+              <p>{fullDictionary.word.definition}</p>
+              <div className="my-2 w-2/5 border-b-2 border-sciquelTeal" />
+              <p className="text-sciquelCitation">In Context</p>
+              {Object.keys(fullDictionary.word.inContext).map((item, index) => (
+                <button
+                  type="button"
+                  className="my-2 text-start"
+                  key={item}
+                  onClick={() => {
+                    fullDictionary.word?.inContext[
+                      item
+                    ].current?.scrollIntoView({
+                      behavior: "instant",
+                      block: "center",
+                    });
+                  }}
+                >
+                  {item}
+                </button>
+              ))}
+
+              <div className="my-2 w-2/5 border-b-2 border-sciquelTeal" />
+              <p className="text-sciquelCitation">Synonyms & Antonyms</p>
+            </div>
+          ) : (
+            Object.keys(fullDictionary.dictionary).map((item, index) => {
+              return (
+                <div
+                  className="px-4 py-2 font-sourceSerif4"
+                  key={`${item}-${index}`}
+                >
+                  <p className="text-sciquelCitation">Term</p>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      const copyWord = Object.assign(
+                        {},
+                        fullDictionary.dictionary[item],
+                      ) as SelectedDefinition;
+                      copyWord.word = item;
+
+                      fullDictionary?.setWord(copyWord);
+                    }}
+                  >
+                    {item}
+                  </button>
+
+                  <div className="mb-2 mt-1 w-2/5 border-b-2 border-sciquelTeal" />
+                  <p className="text-sciquelCitation">Definition</p>
+                  <p className="border-b-2 border-sciquelTeal py-2">
+                    {fullDictionary.dictionary[item].definition}
+                  </p>
+                </div>
+              );
+            })
+          )}
+        </div>
+        {fullDictionary?.word ? (
+          <button
+            type="button"
+            onClick={() => {
+              fullDictionary?.setWord(null);
+            }}
+            className="flex h-8 items-center justify-center bg-sciquelTeal text-center font-sourceSerif4 text-base text-sciquelCardBg"
+          >
+            See more definitions
+          </button>
+        ) : (
+          <div className="flex h-8 items-center justify-center bg-sciquelTeal text-center font-sourceSerif4 text-base text-sciquelCardBg"></div>
         )}
       </div>
-      {selectedItem ? (
-        <button
-          type="button"
-          onClick={() => {
-            setSelectedItem(null);
-          }}
-          className="flex h-8 items-center justify-center bg-sciquelTeal text-center font-sourceSerif4 text-base text-sciquelCardBg"
-        >
-          See more definitions
-        </button>
-      ) : (
-        <div className="flex h-8 items-center justify-center bg-sciquelTeal text-center font-sourceSerif4 text-base text-sciquelCardBg"></div>
-      )}
-    </div>
-  );
+    );
+  }
+
+  return <></>;
 }
---
+++ src/components/story-components/DictionaryWord.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { useContext, useEffect, useRef, type PropsWithChildren } from "react";
+import { DictionaryContext } from "./DictionaryContext";
+
+interface Props {
+  word: string;
+}
+
+function parsePreviousNode(node: ChildNode, fullSentence: string) {
+  // temporary while I don't know what the backend is doing
+  const searchExp = /(?:[.?!]\s+)((?:[^.?!]|(?:\.\d))+)$/;
+
+  switch (node.nodeName) {
+    case "#text":
+    case "SPAN":
+    case "CODE":
+    case "BUTTON":
+      if (node.textContent) {
+        const innerText = searchExp.exec(node.textContent);
+
+        if (innerText && innerText.length > 1) {
+          return innerText[1] + fullSentence;
+        } else if (node.previousSibling) {
+          return parsePreviousNode(
+            node.previousSibling,
+            node.textContent + fullSentence,
+          );
+        }
+        return node.textContent + fullSentence;
+      } else if (node.previousSibling) {
+        return parsePreviousNode(node.previousSibling, fullSentence);
+      }
+      return fullSentence;
+
+    default:
+      console.warn("found previous node of name: ", node.nodeName);
+      if (node.previousSibling) {
+        return parsePreviousNode(node.previousSibling, fullSentence);
+      }
+      return fullSentence;
+  }
+}
+
+function parseNextNode(node: ChildNode, fullSentence: string) {
+  // temporary while I don't know what the backend is doing
+  const searchExp = /^(?:[^.?!]|(?:\.\S))+[.?!]+\s/;
+
+  switch (node.nodeName) {
+    case "#text":
+    case "SPAN":
+    case "CODE":
+    case "BUTTON":
+      if (node.textContent) {
+        const endText = searchExp.exec(node.textContent);
+
+        if (endText) {
+          return fullSentence + endText[0];
+        } else if (node.nextSibling) {
+          return parseNextNode(
+            node.nextSibling,
+            fullSentence + node.textContent,
+          );
+        } else {
+          return fullSentence + node.textContent;
+        }
+      } else if (node.nextSibling) {
+        return parseNextNode(node.nextSibling, fullSentence);
+      }
+
+      return fullSentence;
+    default:
+      console.warn("found next node of name: ", node.nodeName);
+      console.warn(node);
+      if (node.nextSibling) {
+        return parseNextNode(node.nextSibling, fullSentence);
+      }
+      return fullSentence;
+  }
+}
+
+export default function DictionaryWord({
+  word,
+  children,
+}: PropsWithChildren<Props>) {
+  const dictionary = useContext(DictionaryContext);
+
+  const scrollRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    console.log("inside word: ", word);
+    if (dictionary && scrollRef.current) {
+      const newDict = Object.assign({}, dictionary.dictionary);
+      console.log(scrollRef.current);
+
+      // build sentence?
+
+      let fullSentence = scrollRef.current.textContent
+        ? scrollRef.current.textContent
+        : "";
+
+      if (scrollRef.current.previousSibling) {
+        fullSentence = parsePreviousNode(
+          scrollRef.current.previousSibling,
+          fullSentence,
+        );
+      }
+      if (scrollRef.current.nextSibling) {
+        fullSentence = parseNextNode(
+          scrollRef.current.nextSibling,
+          fullSentence,
+        );
+      }
+      console.log("final full sentence is: ", fullSentence);
+
+      if (!newDict[word].inContext[fullSentence]) {
+        newDict[word].inContext[fullSentence] = scrollRef;
+        dictionary.setDictionary(newDict);
+      }
+    }
+  }, []);
+
+  return (
+    <button
+      ref={scrollRef}
+      onClick={() => {
+        if (dictionary && dictionary?.dictionary[word]) {
+          dictionary.setWord({
+            ...dictionary?.dictionary[word],
+            word: word,
+          });
+          dictionary.setOpen(true);
+        }
+      }}
+      type="button"
+      className="fw-bold m-0 rounded-lg border-2 border-sciquelTeal px-2 text-sciquelTeal"
+    >
+      {children ? children : word}
+    </button>
+  );
+}
