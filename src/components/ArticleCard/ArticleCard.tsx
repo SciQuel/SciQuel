5336dd46abbcd3595ece6acdece1e464fe3a64b2
Tahina Andriantomanga <andriantomangat@gmail.com>
2023-10-30 16:55:40 -0400

    resizing article card

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -28,100 +28,102 @@ export default function ArticleCard({
   preferHorizontal = false,
 }: Props) {
   return (
-    <Link href={href ?? "#"}>
-      <div
-        className={clsx(
-          `flex h-full w-full max-w-xs cursor-pointer overflow-clip rounded-lg transition hover:scale-[1.02]`,
-          {
-            "border border-sciquelCardBorder bg-sciquelCardBg": !(
-              mini && preferHorizontal
-            ),
-            "ml-auto w-[175px]": mini && preferHorizontal,
-          },
-          mini && !preferHorizontal ? "min-w-[275px]" : "min-w-[300px]",
-          preferHorizontal ? "flex-row" : "flex-col",
-        )}
-      >
+    <div className="w-full">
+      <Link className="align-middle" href={href ?? "#"}>
         <div
           className={clsx(
-            "flex flex-col gap-4",
-            mini
-              ? preferHorizontal
-                ? "py-2"
-                : "grow  p-3"
-              : preferHorizontal
-              ? "w-2/3 p-3"
-              : "grow  p-3",
+            `flex h-full w-full max-w-sm cursor-pointer overflow-clip rounded-lg transition hover:scale-[1.02]`,
+            {
+              "border border-sciquelCardBorder bg-sciquelCardBg": !(
+                mini && preferHorizontal
+              ),
+              "ml-auto w-[175px]": mini && preferHorizontal,
+            },
+            mini && !preferHorizontal ? "min-w-[275px]" : "min-w-[300px]",
+            preferHorizontal ? "flex-row" : "flex-col",
           )}
         >
-          {/* Article Card Header */}
-          {!mini || !preferHorizontal ? (
-            <div className="flex w-full flex-row">
-              <TopicTag name={topic} />
-              <div className="grow text-right">
-                <p
+          <div
+            className={clsx(
+              "flex flex-col gap-4",
+              mini
+                ? preferHorizontal
+                  ? "py-2"
+                  : "grow  p-3"
+                : preferHorizontal
+                ? "w-2/3 p-3"
+                : "grow  p-3",
+            )}
+          >
+            {/* Article Card Header */}
+            {!mini || !preferHorizontal ? (
+              <div className="flex w-full flex-row">
+                <TopicTag name={topic} />
+                <div className="grow text-right">
+                  <p
+                    className={clsx(
+                      "m-0 text-sciquelMuted",
+                      mini ? "text-sm" : "text-base",
+                    )}
+                  >
+                    ARTICLE
+                  </p>
+                </div>
+              </div>
+            ) : null}
+            {/* Article Content */}
+            <div className="flex grow flex-col">
+              <div>
+                <h1
                   className={clsx(
-                    "m-0 text-sciquelMuted",
-                    mini ? "text-sm" : "text-base",
+                    "line-clamp-2 font-alegreyaSansSC font-medium",
+                    mini
+                      ? preferHorizontal
+                        ? "text-lg leading-tight"
+                        : "text-lg leading-snug"
+                      : "text-xl",
                   )}
                 >
-                  ARTICLE
+                  {title}
+                </h1>
+              </div>
+              <div className="flex items-center">
+                <p className={clsx("line-clamp-3", mini ? "text-xs" : "")}>
+                  {subtitle}
                 </p>
               </div>
             </div>
-          ) : null}
-          {/* Article Content */}
-          <div className="flex grow flex-col">
-            <div>
-              <h1
-                className={clsx(
-                  "line-clamp-2 font-alegreyaSansSC font-medium",
-                  mini
-                    ? preferHorizontal
-                      ? "text-lg leading-tight"
-                      : "text-lg leading-snug"
-                    : "text-xl",
-                )}
-              >
-                {title}
-              </h1>
-            </div>
-            <div className="flex items-center">
-              <p className={clsx("line-clamp-3", mini ? "text-xs" : "")}>
-                {subtitle}
-              </p>
+            <div
+              className={clsx(
+                "flex flex-row justify-between font-sourceSerif4 font-[350] text-sciquelMuted",
+                mini ? "text-xs" : "text-sm",
+              )}
+            >
+              <p>By {author}</p>
+              <p>{date}</p>
             </div>
           </div>
           <div
             className={clsx(
-              "flex flex-row justify-between font-sourceSerif4 font-[350] text-sciquelMuted",
-              mini ? "text-xs" : "text-sm",
+              "relative",
+              mini
+                ? preferHorizontal
+                  ? "hidden"
+                  : "h-24"
+                : preferHorizontal
+                ? "w-1/3"
+                : "h-44",
             )}
           >
-            <p>By {author}</p>
-            <p>{date}</p>
+            <Image
+              src={thumbnailUrl}
+              fill={true}
+              style={{ objectFit: "cover" }}
+              alt={title}
+            />
           </div>
         </div>
-        <div
-          className={clsx(
-            "relative",
-            mini
-              ? preferHorizontal
-                ? "hidden"
-                : "h-24"
-              : preferHorizontal
-              ? "w-1/3"
-              : "h-44",
-          )}
-        >
-          <Image
-            src={thumbnailUrl}
-            fill={true}
-            style={{ objectFit: "cover" }}
-            alt={title}
-          />
-        </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
