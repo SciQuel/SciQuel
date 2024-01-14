3565435a2eeb2f9984fa1abf7f25184cc610a717
kwu0103 <01wkl03@gmail.com>
2024-01-14 16:21:13 -0500

    Add articleseries component

--- src/components/ArticleSeries/index.tsx
+++ src/components/ArticleSeries/index.tsx
@@ -4,33 +4,23 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function ArticleSeries() {
-  const [scroll, setScroll] = useState(false);
-
   return (
-    <div className="top-16 sticky z-20 flex flex-col bg-sciquelTeal text-white">
+    <div className="sticky top-16 z-10 flex flex-col bg-sciquelTeal text-white">
       {/*Articles  */}
       <div
-        className=" duration-350 z-10 flex h-auto flex-row px-40 text-center transition-all"
+        className=" flex h-auto flex-row px-40 text-center"
         // style={{ height: scroll ? "0" : "40px" }}
       >
-        <div
-          className=" grow cursor-pointer py-2 transition-colors hover:bg-sciquelHover"
-          style={{ display: scroll ? "none" : "block" }}
-        >
+        <div className=" grow py-2 text-right ">Name of Series | </div>
+        <div className=" grow cursor-pointer py-2  ">
           <Link href="/">Article 1</Link>
         </div>
 
-        <div
-          className="grow cursor-pointer py-2 transition-colors hover:bg-sciquelHover"
-          style={{ display: scroll ? "none" : "block" }}
-        >
+        <div className="grow cursor-pointer py-2 ">
           <Link href="/">Article 2</Link>
         </div>
 
-        <div
-          className="grow cursor-pointer py-2 transition-colors hover:bg-sciquelHover"
-          style={{ display: scroll ? "none" : "block" }}
-        >
+        <div className="grow cursor-pointer py-2 ">
           <Link href="/">Article 3</Link>
         </div>
       </div>
