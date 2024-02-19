6fd777fc421dbe21bdd8471681b77f73867c800b
ericanderson85 <ericandersonnn@icloud.com>
2024-02-19 15:56:41 -0500

    Center pipe after series name

--- src/components/ArticleSeries/index.tsx
+++ src/components/ArticleSeries/index.tsx
@@ -7,19 +7,23 @@ export default function ArticleSeries() {
     <div className="sticky top-16 z-10 flex flex-col bg-sciquelTeal text-white">
       {/*Articles  */}
       <div
-        className=" flex h-auto flex-row px-40 text-center"
+        className="flex h-auto flex-row px-40 text-center"
         // style={{ height: scroll ? "0" : "40px" }}
       >
-        <div className=" grow py-2 text-right ">Name of Series | </div>
-        <div className=" grow cursor-pointer py-2  ">
+        <div className="flex grow flex-row justify-between py-2">
+          <span className="invisible">|</span>
+          <Link href="/">Name of Series</Link>
+          <span className="opacity-50">|</span>
+        </div>
+        <div className="grow cursor-pointer py-2">
           <Link href="/">Article 1</Link>
         </div>
 
-        <div className="grow cursor-pointer py-2 ">
+        <div className="grow cursor-pointer py-2">
           <Link href="/">Article 2</Link>
         </div>
 
-        <div className="grow cursor-pointer py-2 ">
+        <div className="grow cursor-pointer py-2">
           <Link href="/">Article 3</Link>
         </div>
       </div>
