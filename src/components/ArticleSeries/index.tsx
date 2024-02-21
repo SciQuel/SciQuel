3b6b4a8cf2721e5a9618a9f1401adcdf89455440
ericanderson85 <ericandersonnn@icloud.com>
2024-02-21 17:08:41 -0500

    Series header hidden when scrolling down

--- src/components/ArticleSeries/index.tsx
+++ src/components/ArticleSeries/index.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function ArticleSeries() {
+  const isScrollingUp = useScrollDirection();
+
   return (
-    <div className="sticky top-16 z-10 flex flex-col bg-sciquelTeal text-white">
+    <div
+      className={`sticky z-10 flex flex-col bg-sciquelTeal text-white transition-all duration-500 ${
+        isScrollingUp ? "top-16" : "-top-28"
+      }`}
+    >
       {/*Articles  */}
-      <div
-        className="flex h-auto flex-row px-40 text-center"
-        // style={{ height: scroll ? "0" : "40px" }}
-      >
+      <div className="flex h-auto flex-row px-40 text-center">
         <div className="flex grow flex-row justify-between py-2">
           <span className="invisible">|</span>
           <Link href="/" className="hover:underline">
@@ -37,4 +41,31 @@ export default function ArticleSeries() {
       </div>
     </div>
   );
+}
+
+function useScrollDirection(): boolean {
+  const [isScrollingUp, setIsScrollingUp] = useState<boolean>(true);
+
+  useEffect(() => {
+    let prevScrollY = window.scrollY;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.scrollY;
+      const scrollingUp = scrollY < prevScrollY;
+      // Buffer to exclude small scroll events
+      if (
+        scrollingUp !== isScrollingUp &&
+        Math.abs(scrollY - prevScrollY) > 8
+      ) {
+        setIsScrollingUp(scrollingUp);
+      }
+      prevScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection);
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection);
+    };
+  }, [isScrollingUp]);
+
+  return isScrollingUp;
 }
