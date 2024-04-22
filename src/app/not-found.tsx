8b8acea11e3d1d71d36d7ae17b5fd5b7869b7e1b
ericanderson85 <ericandersonnn@icloud.com>
2024-04-22 17:18:25 -0400

    spacing on header

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex grow flex-col items-center justify-center">
+    <div className="mt-20 flex grow flex-col items-center justify-center">
       <div className="flex flex-row">
         <div className="flex flex-col text-sciquelMuted2">
           <h2 className="text-4xl font-bold">Oops! Page Not Found</h2>
