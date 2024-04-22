bf1dd6e0a14f659ee1d01699dd3a46923b9599ae
ericanderson85 <ericandersonnn@icloud.com>
2024-04-22 17:14:17 -0400

    Series subheader no longer moves

--- src/components/Header/SeriesSubHeader/index.tsx
+++ src/components/Header/SeriesSubHeader/index.tsx
@@ -1,38 +1,44 @@
 "use client";
 
 import Link from "next/link";
-import useScrollDirection from "../useScrollDirection";
 
 export default function SeriesSubHeader() {
-  const isScrollingUp = useScrollDirection();
-
   return (
-    <div
-      className={`sticky flex h-auto flex-row bg-sciquelTeal text-center text-white transition-all duration-500 lg:px-40
-      ${isScrollingUp ? "top-16" : "top-6"}`}
-    >
+    <div className="sticky top-16 flex h-auto flex-row bg-sciquelTeal text-center text-white lg:px-40">
       <div className="flex grow flex-row justify-between py-2">
         <span className="invisible">|</span>
-        <Link href="/" className="hover:underline">
+        <Link
+          href="/"
+          className="decoration-[3px] underline-offset-[3px] hover:underline"
+        >
           Name of Series
         </Link>
-        <span className="opacity-50">|</span>
+        <span className="">|</span>
       </div>
 
       <div className="grow py-2">
-        <Link href="/" className="hover:underline">
+        <Link
+          href="/"
+          className="decoration-[3px] underline-offset-[3px] hover:underline"
+        >
           Article 1
         </Link>
       </div>
 
       <div className="grow py-2">
-        <Link href="/" className="hover:underline">
+        <Link
+          href="/"
+          className="decoration-[3px] underline-offset-[3px] hover:underline"
+        >
           Article 2
         </Link>
       </div>
 
       <div className="grow py-2">
-        <Link href="/" className="hover:underline">
+        <Link
+          href="/"
+          className="decoration-[3px] underline-offset-[3px] hover:underline"
+        >
           Article 3
         </Link>
       </div>
