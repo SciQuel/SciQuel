04a088c38076bc80e2c5c0ca46328c39acfcd822
Daniel Kim <daniel.j.kim164@gmail.com>
2024-04-22 22:24:31 -0400

    create initial working definitions POST

--- src/app/api/stories/definitions/route.ts
+++ src/app/api/stories/definitions/route.ts
@@ -1,34 +1,58 @@
 import prisma from "@/lib/prisma";
-import { NextResponse, type NextRequest } from "next/server";
-import { z } from "zod";
+import { Storage } from "@google-cloud/storage";
+import { NextResponse } from "next/server";
 import { postDefinitionSchema } from "./schema";
 
-export async function POST(req: NextRequest) {
+const storage = new Storage({
+  projectId: process.env.GCS_PROJECT ?? "",
+  credentials: {
+    client_email: process.env.GCS_CLIENT ?? "",
+    private_key: process.env.GCS_KEY ?? "",
+  },
+});
+
+// TODO: need to prevent file upload before prisma errors (how?)
+// checks for duplicate uploads, user permissions?
+export async function POST(req: NextResponse) {
+  const parsedData = postDefinitionSchema.safeParse(await req.formData());
+  console.log(parsedData);
+  if (!parsedData.success) {
+    console.error(parsedData.error);
+    return NextResponse.json({ error: "Bad Request" }, { status: 400 });
+  }
   try {
-    const requestBody = postDefinitionSchema.safeParse(await req.json());
-    if (!requestBody.success) {
-      return NextResponse.json(
-        { errors: requestBody.error.format() },
-        { status: 400 },
-      );
-    }
-    // retrieve data
-    const data = requestBody.data;
-    // check if story exists
-    const storyExists = await prisma.story.findUnique({
-      where: { id: data.storyId },
-    });
-    if (!storyExists) {
-      return NextResponse.json({ error: "Story not found" }, { status: 404 });
-    }
-    // create dictionary definition
-    const definition = await prisma.dictionaryDefinition.create({
+    const newDefinition = await prisma.dictionaryDefinition.create({
       data: {
-        ...data,
+        word: parsedData.data.word,
+        definition: parsedData.data.definition,
+        exampleSentences: parsedData.data.exampleSentences, // Need to fix 'any' warning (zod)
+        alternativeSpellings: parsedData.data.alternativeSpellings, // Need to fix 'any' warning (zod)
+        storyId: parsedData.data.storyId,
+        wordAudioUrl: await uploadFile(parsedData.data.wordAudio),
+        definitionAudioUrl: await uploadFile(parsedData.data.definitionAudio),
+        useAudioUrl: await uploadFile(parsedData.data.usageAudio),
       },
     });
-    return NextResponse.json(definition, { status: 201 });
+    return newDefinition;
   } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
+    console.error("Failed to save to DB:", error);
+    throw new Error("Database operation failed");
+  }
+}
+
+// TODO: definitely need more checks for file types, size, etc
+async function uploadFile(file: File) {
+  try {
+    const buffer = await file.arrayBuffer();
+    const fileName = `${Date.now()}-${file.name}`;
+    const fileUrl = `https://storage.googleapis.com/sciquel-dictionary-audio/${fileName}`;
+    await storage
+      .bucket("sciquel-dictionary-audio")
+      .file(fileName)
+      .save(Buffer.from(buffer));
+    return fileUrl;
+  } catch (error) {
+    console.error("File failed to upload to GCS", error);
+    throw new Error("Failed to upload file");
   }
 }
