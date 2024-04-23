54867410c08fdc9fdac07abeb981453410f16bfd
Daniel Kim <daniel.j.kim164@gmail.com>
2024-04-22 22:23:19 -0400

    update validation schema for multipart form data

--- src/app/api/stories/definitions/schema.ts
+++ src/app/api/stories/definitions/schema.ts
@@ -1,14 +1,34 @@
 import { z } from "zod";
+import { zfd, type json } from "zod-form-data";
 
-export const postDefinitionSchema = z.object({
-  word: z.string(),
-  definition: z.string(),
-  exampleSentences: z.array(z.string()),
-  alternativeSpellings: z.array(z.string()),
-  storyId: z
-    .string({
-      required_error: "story_id is required",
-      invalid_type_error: "story_id must be a ObjectId",
+// TODO: change alternativeSpellings and exampleSentence type
+// to avoid 'any' warning
+export const postDefinitionSchema = zfd.formData({
+  word: zfd.text(),
+  definition: zfd.text(),
+  exampleSentences: zfd
+    .text()
+    .transform((str, ctx): z.infer<ReturnType<typeof json>> => {
+      try {
+        return JSON.parse(str);
+      } catch (e) {
+        ctx.addIssue({ code: "custom", message: "Invalid JSON" });
+        return z.NEVER;
+      }
+    }),
+  alternativeSpellings: zfd
+    .text()
+    .transform((str, ctx): z.infer<ReturnType<typeof json>> => {
+      try {
+        return JSON.parse(str);
+      } catch (e) {
+        ctx.addIssue({ code: "custom", message: "Invalid JSON" });
+        return z.NEVER;
+      }
     })
-    .regex(/^[0-9a-f]{24}$/, { message: "story_id must be a valid ObjectId" }),
+    .optional(),
+  storyId: zfd.text(),
+  wordAudio: zfd.file(),
+  definitionAudio: zfd.file(),
+  usageAudio: zfd.file(),
 });
