fdefa3dcd1673bd796de412cd8dc9791f54b4d4f
aidmccor <aidmccor@iu.edu>
2024-06-15 18:32:34 -0500

    .

--- src/components/story-components/StoryImagePopup.tsx
+++ src/components/story-components/StoryImagePopup.tsx
@@ -10,17 +10,17 @@ const StoryImagePopup = ({ src, children, handleClick, imageRef, alt}) => {
   const [imageClicked, setImageClicked] = useState(false);
   const [imagePosition, setImagePosition] = useState({ x: null, y: null });
   const [dragging, setDragging] = useState(false);
-  const [scaleLevel, setScaleLevel] = useState(1.5);
+  const [scaleLevel, setScaleLevel] = useState(1);
 
   const handleImageClick = () => {
-    if (scaleLevel === 3) {
+    if (scaleLevel === 4) {
       setImageClicked(false);
-      console.log(imageClicked);
-      setScaleLevel(1.5);
+      setScaleLevel(1);
       return;
     }
 
     if (!imageClicked) {
+      
       setImageClicked(true);
     } else {
       setScaleLevel(scaleLevel + 1.5);
@@ -35,6 +35,17 @@ const StoryImagePopup = ({ src, children, handleClick, imageRef, alt}) => {
     setImagePosition({ x, y });
   };
 
+  const transformOriginValue = imagePosition.x !== null
+  ? `${imagePosition.x}px ${imagePosition.y}px`
+  : "center center"
+
+  const transformValue = imageClicked ? `scale(${scaleLevel})` : "none"
+
+  const imageStyles = {
+    transformOrigin: transformOriginValue,
+    transform: transformValue
+  }
+
   return (
     <div
       className={`fixed left-1/2 top-1/2 z-50 flex h-screen w-screen -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center border border-solid border-slate-800 bg-white hover:cursor-pointer`}
@@ -45,24 +56,15 @@ const StoryImagePopup = ({ src, children, handleClick, imageRef, alt}) => {
         <img
           src={src}
           className={` relative mx-auto max-h-[700px] ${scaleLevel < 3 ? "hover:cursor-zoom-in" : "hover:cursor-zoom-out"}`}
-
           ref={imageRef}
           onClick={handleImageClick}
           onMouseMove={handleImageDrag}
           alt = {alt}
-          style={{
-            transformOrigin: `${
-              imagePosition.x !== null
-                ? `${imagePosition.x}px ${imagePosition.y}px`
-                : "center center"
-            }`,
-            transform: imageClicked ? `scale(${scaleLevel})` : "none",
-            backgroundColor : 'blue',
-          }}
-        />
+          style={imageStyles}
+            />
       
 
-      <p> {children} </p>
+      {!imageClicked && <p> {children} </p>}
       </div>
       
 
