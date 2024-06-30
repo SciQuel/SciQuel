9406f47a35f6de116893ce96a273d0ec1bb88275
Hillary764 <105897581+Hillary764@users.noreply.github.com>
2024-06-30 15:34:35 -0400

    small fix

--- src/components/WhatsNewSection/index.tsx
+++ src/components/WhatsNewSection/index.tsx
@@ -33,7 +33,11 @@ export default function WhatsNewSection({ articles }: Props) {
               headlineArticle.publishedAt,
             ).toLocaleString(DateTime.DATE_FULL)}
             mediaType={headlineArticle.storyType}
-            tag={headlineArticle.topics[0]}
+            tag={
+              headlineArticle.topics && headlineArticle.topics[0]
+                ? headlineArticle.topics[0]
+                : "BIOLOGY"
+            }
             href={(() => {
               const publishDate = DateTime.fromJSDate(
                 headlineArticle.publishedAt,
