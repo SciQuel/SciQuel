28c928f95374a5b4c7adbfa2c8dd4b6d152dbe66
Hillary764 <105897581+Hillary764@users.noreply.github.com>
2024-06-30 08:39:32 -0400

    footer resizing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,14 +5,17 @@ export default function Footer() {
   return (
     <div className=" z-10 flex w-full flex-col bg-sciquelFooter text-white ">
       <div className="relative">
-        <div className=" flex w-full flex-row justify-between gap-4 px-10 py-4 align-middle">
-          <div className="flex flex-col gap-4 pt-4 ">
+        <div className=" flex w-full flex-row justify-center gap-4 px-4 pb-4 align-middle xs:justify-between sm:px-10 sm:pt-4">
+          <div className="hidden flex-col gap-4 pt-4 xs:flex">
             <p>Get a weekly dose of accessible science to your inbox</p>
-            <form>
+            <form className="w-full">
               <input
                 id="email"
                 type="email"
-                className="h-9 w-64 rounded-l-lg border border-solid bg-sciquelFooter pl-2 text-xs font-medium text-sciquelFooter placeholder-white outline-1"
+                className="h-9 w-full rounded-l-lg rounded-r-lg border border-solid
+                 bg-sciquelFooter pl-2 text-xs font-medium
+                  text-sciquelFooter placeholder-white outline-1
+                   sm:w-64 sm:rounded-r-none"
                 placeholder="team@sciquel.org"
                 pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                 title="Please provide a valid email"
@@ -20,14 +23,18 @@ export default function Footer() {
               />
               <button
                 type="submit"
-                className="h-9 w-28 rounded-r-lg bg-white text-xs text-sciquelFooter"
+                className="mx-auto mt-3 h-9 w-28 
+                rounded-l-lg rounded-r-lg
+                 bg-white text-xs text-sciquelFooter sm:mx-0 sm:mt-0 sm:rounded-l-none"
               >
                 Subscribe
               </button>
             </form>
           </div>
           <div className="flex flex-col items-end pt-4 ">
-            <p>Sciquel</p>
+            <p className="w-full text-center text-lg xs:w-auto xs:text-end xs:text-base">
+              Sciquel
+            </p>
             <div className="flex flex-row">
               <FooterIcon type="instagram" />
               <FooterIcon type="facebook" />
@@ -40,7 +47,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="mb-4 flex w-full justify-center">
-        <nav className="flex gap-3">
+        <nav className="flex flex-wrap justify-center gap-3">
           <Link href="/">About</Link>
           <Link href="/">Team</Link>
           <Link href="/">Terms & Conditions</Link>
