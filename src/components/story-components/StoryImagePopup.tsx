3bbe507e7de600cb673a33ffe9c6016a58115f54
aidmccor <aidmccor@iu.edu>
2024-07-14 22:14:55 -0500

    Revert "disabled click to zoom on smaller devices, and other fixes"
    This reverts commit
    3fae23b5883a606dc080a682bac9b0dad06e1fa8.

--- src/components/story-components/StoryImagePopup.tsx
+++ src/components/story-components/StoryImagePopup.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, PropsWithChildren, RefObject, useState, useEffect} from "react";
+import { MouseEvent, PropsWithChildren, RefObject, useState} from "react";
 
 interface Props {
   src: string;
@@ -24,27 +24,12 @@ const StoryImagePopup = ({
   }>({ x: null, y: null });
   const [dragging, setDragging] = useState(false);
   const [scaleLevel, setScaleLevel] = useState(1);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
-  /* created window event listener to be able to set when viewport is mobile width */
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-    
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   //handler for when you click on popup image, clicking once will get rid of caption and focus on image, clicking another will zoom in
-  const handlePopUpImageClick = () => {
-    if(!isMobile){
-    
+  const handleImageClick = () => {
+
     if (scaleLevel === 3) {
-   
+      setImageClicked(false);
       setScaleLevel(1);
       return;
     }
@@ -52,9 +37,8 @@ const StoryImagePopup = ({
     if (!imageClicked) {
       setImageClicked(true);
     } else {
-      setScaleLevel(prevScaleLevel  => prevScaleLevel + 2);
+      setScaleLevel(scaleLevel + 2);
     }
-  }
   };
 
   //zoom and look feature(ask to keep it , was something quick i figured out)
@@ -78,48 +62,38 @@ const StoryImagePopup = ({
   const imageStyles = {
     transformOrigin: transformOriginValue,
     transform: transformValue,
-    cursor : isMobile
-    ? 'default'
-    : scaleLevel === 1
-    ? 'zoom-in'
-    : 'zoom-out'
-    
   };
 
   return (
-    
     <div
       className={`fixed left-1/2 top-1/2 z-50 flex h-screen w-screen -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center border border-solid border-slate-800 bg-white hover:cursor-pointer`}
       onClick={handleClick}
     >
 
-        {/* container for content inside popup */}
-      <div className="  flex flex-wrap justify-center items-center  z-0 border-solid">
+      
+      <div className=" flex flex-wrap items-center justify-center gap-4 z-0">
 
-          
-            {/* image container */}
-            <div className = '  max-w-[95%] w-[700px]  max-h-[700px] '>
+            {/* <div className = 'border border-solid border-slate-700 w-[700px] '> */}
         <img
           src={src}
-          className={` w-full ml-auto block relative  max-h-[700px] object-contain border border-solid border-sciquelHover`}
+          className={` max-w-[95%] w-[700px] block relative mx-auto max-h-[700px] object-contain z-50  h-full   ${
+            scaleLevel === 1 ? "hover:cursor-zoom-in" : "hover:cursor-zoom-out"
+          }`}
           ref={imageRef}
-          onClick={handlePopUpImageClick}
+          onClick={handleImageClick}
           onMouseMove={handleImageDrag}
           alt={alt}
           style={imageStyles}
         />
-       
-        </div>
  
-        {/* caption - hide the caption when the image is clicked*/}
-        { <p className = {` w-[30%] text-center text-wrap cursor-default ${imageClicked ? 'hidden' : ''}`} ref = {captionRef} > {children} </p>}
+        {!imageClicked && <p className = 'text-center' ref = {captionRef} > {children} </p>}
       </div>
 
       <button
         aria-label="close popup"
         className="absolute right-0 top-0 mr-5 mt-3 text-3xl"
       >
-        &times;
+        x
       </button>
     </div>
   );
