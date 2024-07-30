75f2d76f655deeb1f911ab60466868e22b378441
00eemsy <eyu19415@gmail.com>
2024-07-30 14:56:06 -0700

    incomplete but updating user-dash

--- src/components/UserDashboard/BrainedArticlesBox.tsx
+++ src/components/UserDashboard/BrainedArticlesBox.tsx
@@ -15,55 +15,55 @@ export default function BrainedArticleBox() {
 
   const [returnVal, setReturnVal] = useState(<p>Loading...</p>);
 
-  const response = fetch(URL, {
-    method: "GET",
-    // add in user_email
-  })
-    .then(response => {
-      console.log(`response...?: `, response);
-      return response.text();
-    })
+  // const response = fetch(URL, {
+  //   method: "GET",
+  //   // add in user_email
+  // })
+  //   .then(response => {
+  //     console.log(`response...?: `, response);
+  //     return response.json();
+  //   })
 
-  const latestBrainedArticle = response.then((data) => {
-    const storyVal = data as Stories;
-    console.log(`storyVal on brained!: `, storyVal);
-    return storyVal as Stories
-  })
+  // const latestBrainedArticle = response.then((data) => {
+  //   const storyVal = data as Stories;
+  //   console.log(`storyVal on brained!: `, storyVal);
+  //   return storyVal as Stories
+  // })
 
-  let headlineArticle: Story;
+  // let headlineArticle: Story;
   
-  useEffect(() => {
-    latestBrainedArticle.then((data) => {
-      headlineArticle = (data as Stories)?.[0];
-    })
+  // useEffect(() => {
+  //   latestBrainedArticle.then((data) => {
+  //     headlineArticle = (data as Stories)?.[0];
+  //   })
 
-    let temp = (
-        <div className="flex items-center">
-          <Image
-            src={headlineArticle.thumbnailUrl}
-            fill={false}
-            width={`${50}`}
-            height={`${50}`}
-            alt="bobtail"
-            className="rounded-md aspect-square h-12 w-12 mx-3"
-            style={{ position: "relative"}}
-          />
+  //   let temp = (
+  //       <div className="flex items-center">
+  //         <Image
+  //           src={headlineArticle.thumbnailUrl}
+  //           fill={false}
+  //           width={`${50}`}
+  //           height={`${50}`}
+  //           alt="bobtail"
+  //           className="rounded-md aspect-square h-12 w-12 mx-3"
+  //           style={{ position: "relative"}}
+  //         />
   
   
-          <div className="flex justify-between w-full items-center">
-            <div className="flex flex-col mx-3">
-              <h2 className="text-sm font-semibold">{headlineArticle.title}</h2>
-              <p className="text-xs mt-1 text-[#696969]">By Jane Smith</p>
-              <p className="text-[.6rem] mt-1 text-[#9E9E9E]">Saved March 2, 2024</p>
-            </div>
+  //         <div className="flex justify-between w-full items-center">
+  //           <div className="flex flex-col mx-3">
+  //             <h2 className="text-sm font-semibold">{headlineArticle.title}</h2>
+  //             <p className="text-xs mt-1 text-[#696969]">By Jane Smith</p>
+  //             <p className="text-[.6rem] mt-1 text-[#9E9E9E]">Saved March 2, 2024</p>
+  //           </div>
   
-            <p className="text-[.6rem] mx-3 bg-[#E3954F] rounded-full py-.5 px-1.5 text-white">BIOLOGY</p>
-          </div>
-        </div>
-      )
+  //           <p className="text-[.6rem] mx-3 bg-[#E3954F] rounded-full py-.5 px-1.5 text-white">BIOLOGY</p>
+  //         </div>
+  //       </div>
+  //     )
 
-      setReturnVal(temp);
-  })
+  //     setReturnVal(temp);
+  // })
 
 
   // old code ⬇️
