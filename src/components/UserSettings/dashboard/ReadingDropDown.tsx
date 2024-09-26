fc5691d26153bb2dd3588bbb6b38a2b5d4204cd4
aidmccor <aidmccor@iu.edu>
2024-09-26 12:27:07 -0400

    add arrows

--- src/components/UserSettings/dashboard/ReadingDropDown.tsx
+++ src/components/UserSettings/dashboard/ReadingDropDown.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import Bookmark from '../../../../public/assets/images/bookmark-final.svg'
 import Lightbulb from '../../../../public/assets/images/oi-lightbulb.svg'
 import Share from '../../../../public/assets/images/oi-share-alt.svg'
+import ArrowDown from '../../../../public/assets/images/oi-chevron-down.svg'
 import axios from 'axios'
 import env from '@/lib/env'
 
@@ -12,13 +13,14 @@ const ReadingDropDown = ({ data, title, email, userId }) => {
 
 
   //get list of all stories the user has bookmarked
+  //can this part be on the backend, or should i do the logic to find what stories are bookmarked on front
   useEffect(() => {
     const getBookMarkedReadings = async () => {
       try {
         const response = await axios.get(`${env.NEXT_PUBLIC_SITE_URL}/api/user/bookmark/latest`, {
           params: {
             user_email: email,
-            page_size: data.length
+            page_size: data.length //what can i pass as page size parameter
           }
         })
         const bookmarkedReadings = response.data
@@ -58,13 +60,18 @@ const ReadingDropDown = ({ data, title, email, userId }) => {
 
   return (
     <>
-      <div className="border-b-2 w-full cursor-pointer h-[45px]" onClick={() => setIsOpen(prev => !prev)}> {title} </div>
+      <div className=" border-b-2 w-full  h-[45px] flex cursor-pointer" onClick={() => setIsOpen(prev => !prev)}> {title}
+        <button className='ml-auto mr-5'>
+          <ArrowDown transform={isOpen ? 'rotate(180)' : ''} />
+        </button>
+      </div>
 
       {isOpen && (
         <div className='max-h-[500px] overflow-y-scroll scrollbar-cyan'>
           {data.map((reading) => (
+
             <ul className='mt-5' key={reading.storyId}>
-              <div className='flex items-center gap-7'>
+              <div className='flex  items-center gap-7'>
                 <img src={reading.story.thumbnailUrl} alt={`Thumbnail of ${reading.story.title}`} className='w-20 h-20 object-cover' />
                 <div>
                   <p className='font-bold'>{reading.story.title}</p>
@@ -72,16 +79,26 @@ const ReadingDropDown = ({ data, title, email, userId }) => {
                   <p className='text-sm font-light'>{`Viewed ${reading.diffInDays} days ago`}</p>
                 </div>
 
+
+
+
                 {/* Icons */}
-                <div className='flex ml-auto items-center mr-5 '>
-                  <Bookmark fill={`${bookMarkedReadings.includes(reading.storyId) ? 'yellow' : 'none'}`} width='50px' height='20px' onClick={() => handleBookmarkClick(reading.storyId)} />
-                  <Lightbulb width='50px' height='20px' />
-                  <Share width='50px' height='20px' />
+                <div className='flex ml-auto items-center mr-5 ' >
+                  <button className='cursor-pointer'>
+                    <Bookmark fill={`${bookMarkedReadings.includes(reading.storyId) ? 'yellow' : 'none'}`} width='50px' height='20px' onClick={() => handleBookmarkClick(reading.storyId)} />
+                  </button>
+                  <button className='cursor-pointer'>
+                    <Lightbulb width='50px' height='20px' />
+                  </button>
+                  <button className='cursor-pointer'>
+                    <Share width='50px' height='20px' />
+                  </button>
                 </div>
-              </div>
+              </div >
+
             </ul>
           ))}
-        </div>
+        </div >
       )}
     </>
   )
