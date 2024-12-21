5170e989ef8f5e77e4feda958bdeec31f6e9f1bd
Hillary764 <105897581+Hillary764@users.noreply.github.com>
2024-12-21 18:18:45 -0500

    Package Updates (#83)
    * save
    
    * packages
    
    * eslintrc
    
    * small updates
    
    * small fixes
    
    * remove unused file

--- src/app/api/grade/route.ts
+++ src/app/api/grade/route.ts
@@ -61,45 +61,62 @@ export async function POST(req: NextRequest) {
     // console.log("Existing firstQuizRecord:", firstQuizRecord);
     // console.log("Existing mostRecentQuizRecord:", mostRecentQuizRecord);
 
-      interface ResponseSubpart {
-        id: string;
-        subpartId: string;
-        subpartUserAns: string[];
-      }
+    interface ResponseSubpart {
+      id: string;
+      subpartId: string;
+      subpartUserAns: string[];
+    }
 
-      // find corresponding subpart
-      const gradeResults = await Promise.all(
-        // eslint-disable-next-line @typescript-eslint/require-await
-        responseSubparts.map(async (responseSubpart: { subpartId: string; subpartUserAns: string[]; }) => {
-            const correspondingSubpart = quizQuestion.subparts.find(subpart => subpart.id === responseSubpart.subpartId);
-            if (!correspondingSubpart || !correspondingSubpart.correctAnswer) {
-                return { error: "Subpart not found or correctAnswer is undefined", subpartId: responseSubpart.subpartId };
-            }
+    // find corresponding subpart
+    const gradeResults = await Promise.all(
+      // eslint-disable-next-line @typescript-eslint/require-await
+      responseSubparts.map(
+        (responseSubpart: { subpartId: string; subpartUserAns: string[] }) => {
+          const correspondingSubpart = quizQuestion.subparts.find(
+            (subpart) => subpart.id === responseSubpart.subpartId,
+          );
+          if (!correspondingSubpart || !correspondingSubpart.correctAnswer) {
+            return {
+              error: "Subpart not found or correctAnswer is undefined",
+              subpartId: responseSubpart.subpartId,
+            };
+          }
 
-            if (!responseSubpart.subpartUserAns) {
-                return { error: "User response is undefined", subpartId: responseSubpart.subpartId };
-            }
+          if (!responseSubpart.subpartUserAns) {
+            return {
+              error: "User response is undefined",
+              subpartId: responseSubpart.subpartId,
+            };
+          }
 
-            const isCorrect = JSON.stringify(responseSubpart.subpartUserAns.sort()) === JSON.stringify(correspondingSubpart.correctAnswer.sort());
-            return { subpartId: responseSubpart.subpartId, isCorrect: isCorrect, explanation: correspondingSubpart.explanation, userResponses:responseSubpart.subpartUserAns};
-        })
+          const isCorrect =
+            JSON.stringify(responseSubpart.subpartUserAns.sort()) ===
+            JSON.stringify(correspondingSubpart.correctAnswer.sort());
+          return {
+            subpartId: responseSubpart.subpartId,
+            isCorrect: isCorrect,
+            explanation: correspondingSubpart.explanation,
+            userResponses: responseSubpart.subpartUserAns,
+          };
+        },
+      ),
     );
 
-    const updatedFirstQuizRecord = user.firstQuizRecord 
-  ? (typeof user.firstQuizRecord === 'string' 
-    ? JSON.parse(user.firstQuizRecord) 
-    : user.firstQuizRecord)
-  : {};
-const updatedMostRecentQuizRecord = user.mostRecentQuizRecord 
-  ? (typeof user.mostRecentQuizRecord === 'string' 
-    ? JSON.parse(user.mostRecentQuizRecord) 
-    : user.mostRecentQuizRecord)
-  : {};
+    const updatedFirstQuizRecord = user.firstQuizRecord
+      ? typeof user.firstQuizRecord === "string"
+        ? JSON.parse(user.firstQuizRecord)
+        : user.firstQuizRecord
+      : {};
+    const updatedMostRecentQuizRecord = user.mostRecentQuizRecord
+      ? typeof user.mostRecentQuizRecord === "string"
+        ? JSON.parse(user.mostRecentQuizRecord)
+        : user.mostRecentQuizRecord
+      : {};
 
     // console.log("Existing firstQuizRecord:", updatedFirstQuizRecord);
     // console.log("Existing mostRecentQuizRecord:", updatedMostRecentQuizRecord);
 
-    gradeResults.forEach(grade => {
+    gradeResults.forEach((grade) => {
       if (!updatedFirstQuizRecord.hasOwnProperty(grade.subpartId)) {
         updatedFirstQuizRecord[grade.subpartId] = {
           isCorrect: grade.isCorrect,
@@ -111,20 +128,25 @@ const updatedMostRecentQuizRecord = user.mostRecentQuizRecord
         userResponses: grade.userResponses,
       };
     });
-    
-    console.log("Updated firstQuizRecord before saving:", updatedFirstQuizRecord);
-    console.log("Updated mostRecentQuizRecord before saving:", updatedMostRecentQuizRecord);
+
+    console.log(
+      "Updated firstQuizRecord before saving:",
+      updatedFirstQuizRecord,
+    );
+    console.log(
+      "Updated mostRecentQuizRecord before saving:",
+      updatedMostRecentQuizRecord,
+    );
 
     const updateUser = await prisma.user.update({
       where: { id: userId },
       data: {
         firstQuizRecord: JSON.stringify(updatedFirstQuizRecord),
-      mostRecentQuizRecord: JSON.stringify(updatedMostRecentQuizRecord),
+        mostRecentQuizRecord: JSON.stringify(updatedMostRecentQuizRecord),
       },
     });
-    
+
     //console.log("User after update:", updateUser);
-  
 
     return new NextResponse(JSON.stringify({ gradeSubpart: gradeResults }), {
       status: 200,
@@ -143,7 +165,7 @@ const updatedMostRecentQuizRecord = user.mostRecentQuizRecord
 }
 
 export async function GET(req: NextRequest) {
-  const userId = req.nextUrl.searchParams.get('userId');
+  const userId = req.nextUrl.searchParams.get("userId");
 
   if (!userId) {
     return new NextResponse(JSON.stringify({ error: "User ID is required" }), {
@@ -168,13 +190,16 @@ export async function GET(req: NextRequest) {
       });
     }
 
-    return new NextResponse(JSON.stringify({
-      firstQuizRecord: user.firstQuizRecord,
-      mostRecentQuizRecord: user.mostRecentQuizRecord,
-    }), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
+    return new NextResponse(
+      JSON.stringify({
+        firstQuizRecord: user.firstQuizRecord,
+        mostRecentQuizRecord: user.mostRecentQuizRecord,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
   } catch (error) {
     console.error("Error processing request:", error);
     return new NextResponse(
