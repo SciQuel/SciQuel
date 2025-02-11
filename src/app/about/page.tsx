90de5840143143b71a472fcff02ff8bb3d476ae6
edwrd-chen <95104320+edwrd-chen@users.noreply.github.com>
2025-02-11 04:39:13 +0000

    Updated our mission statement on the about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,24 +7,42 @@ export default function AboutPage() {
       <div className="my-8 px-12">
         <div className="flex flex-col gap-3 text-lg lg:mx-auto lg:max-w-screen-md xl:max-w-screen-lg">
           <p>
-            SciQuel is a new and growing non-profit science media organization.
-            We are scientists, developers, writers, and illustrators who create
-            original content (articles, podcasts, and videos) meant for a
-            non-technical audience. We are passionate about{" "}
-            <span className="font-bold">
-              making science understandable for the general public
-            </span>{" "}
-            through the lens of scientists.
+            SciQuel is a new and growing non-profit science media organization
+            that publishes science writing for the general public. We are
+            passionate about making science understandable for a wide and
+            not-always-technical audience, through the lens of scientists.
           </p>
           <p>
             Our mission is to benefit society by making scientific knowledge
-            freely accessible. We want to truly change the world by inspiring
-            people to think and talk about science on a daily basis.
+            freely accessible. We want to
+            <strong>truly change the world</strong>
+            by inspiring people to think and talk about science on a daily
+            basis—akin to how we collectively ponder sports or pop culture or
+            the weather.
           </p>
           <p>
-            If you are interested in helping out, learning more, or just
-            connecting with a community of like-minded people, reach out to
-            <a href="mailto:team@sciquel.org"> team@sciquel.org!</a>
+            We are
+            <strong>scientists</strong>
+            who know what it’s like to do research; we are
+            <strong>writers</strong>
+            who know what it’s like to write; we are
+            <strong>artists</strong>, <strong>editors</strong>,{" "}
+            <strong>engineers</strong>, and <strong>web developers</strong> who
+            share the value that{" "}
+            <strong>
+              scientific knowledge is best placed in the public domain
+            </strong>
+            and the vision that society will come to take scientific literacy
+            for granted. We believe that science can be taught, no matter how
+            complicated, and that knowledge is worth expounding, no matter the
+            vogue.
+          </p>
+          <p>
+            If you are interested in writing or otherwise helping out, why not
+            join a meaningful cause while exploring what excites you. Whether
+            you want to learn more or simply wish to connect with a community of
+            like-minded people, reach out to us at{" "}
+            <a href="mailto:team@sciquel.org"> team@sciquel.org</a>!
           </p>
         </div>
       </div>
