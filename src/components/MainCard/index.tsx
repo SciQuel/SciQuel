7224822d2364339f9e38ddb678259728bc6af995
Will Saulnier <wsaulnier1004@gmail.com>
2025-12-08 20:13:18 -0500

    main card behavior fix

--- src/components/MainCard/index.tsx
+++ src/components/MainCard/index.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { type StoryTopic, type StoryType } from "@prisma/client";
 import Image from "next/image";
-import React from "react";
-import InnerCard from "./InnerCard";
 import Link from "next/link";
+import React, { useState } from "react";
+import InnerCard from "./InnerCard";
 
 interface Props {
   title: string;
@@ -25,27 +27,37 @@ export default function MainCard({
   thumbnailUrl,
   href,
 }: Props) {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <div
-      className={`relative z-0 mx-auto 
-                  pt-12 text-center
-                  transition-all duration-300
-                   hover:scale-[1.03] xs:min-w-[300px] md:h-[70vh] md:pt-0  xl:min-h-[300px]`}
+      className={`
+        relative z-0 mx-auto pt-12 text-center
+        transition-all duration-300
+        ${hovered ? "scale-[1.03]" : ""}
+        xs:min-w-[300px] md:h-[70vh] md:pt-0 xl:min-h-[300px]
+      `}
     >
-      <Link href={href}>
+      <Link
+        href={href}
+        className={`absolute right-0 top-0 z-10 h-full 
+                    w-full justify-center bg-cover bg-center transition-all duration-300 lg:w-8/12`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <Image
+          src={thumbnailUrl}
+          fill={true}
+          alt={title}
+          className="rounded-md"
+          style={{ objectFit: "cover" }}
+        />
+      </Link>
+      <div className="relative flex h-full w-full flex-col justify-end p-5 transition-all duration-300 lg:w-3/5 lg:justify-center">
         <div
-          className={`absolute right-0 top-0 z-10 h-full 
-                      w-full justify-center bg-cover bg-center transition-all duration-300 lg:w-8/12`}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
         >
-          <Image
-            src={thumbnailUrl}
-            fill={true}
-            alt={title}
-            className="rounded-md"
-            style={{ objectFit: "cover" }}
-          />
-        </div>
-        <div className="relative flex h-full w-full flex-col justify-end p-5 transition-all duration-300 lg:w-3/5 lg:justify-center">
           <InnerCard
             href={href}
             title={title}
@@ -55,13 +67,13 @@ export default function MainCard({
             mediaType={mediaType}
             tag={tag}
           />
-          {/* <div className="z-10 mx-5 my-3 hidden text-left transition-all duration-300 lg:relative lg:top-16 lg:block">
-            <p className="font-sourceSerif4 text-xl font-[350] text-sciquelTeal">
-              {author} | {date}
-            </p>
-          </div> */}
         </div>
-      </Link>
+        {/* <div className="z-10 mx-5 my-3 hidden text-left transition-all duration-300 lg:relative lg:top-16 lg:block">
+          <p className="font-sourceSerif4 text-xl font-[350] text-sciquelTeal">
+            {author} | {date}
+          </p>
+        </div> */}
+      </div>
     </div>
   );
 }
