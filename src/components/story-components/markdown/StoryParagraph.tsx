ad40acb67585ee126a25018e53af770c11a83b13
Lucas11011 <lnaurelio219@gmail.com>
2026-01-02 14:25:52 -0500

    Using Tailwind classes instead of globals.css

--- src/components/story-components/markdown/StoryParagraph.tsx
+++ src/components/story-components/markdown/StoryParagraph.tsx
@@ -4,9 +4,8 @@ export default function StoryParagraph({
   children,
 }: PropsWithChildren<unknown>) {
   return (
-    <p className="story-paragraph mx-0 w-full font-sourceSerif4 text-lg font-[370] leading-8 md:w-[768px] first:first-letter:font-customTest first:first-letter:text-[5rem] first:first-letter:leading-[54px] first:first-letter:float-left first:first-letter:mr-[0.3rem] first:mt-6">
+    <p className="story-paragraph mx-0 w-full font-sourceSerif4 text-lg font-[370] leading-8 first:mt-6 first:first-letter:float-left first:first-letter:mr-[0.3rem] first:first-letter:font-customTest first:first-letter:text-[5rem] first:first-letter:leading-[54px] md:w-[768px]">
       {children}
     </p>
-
   );
 }
