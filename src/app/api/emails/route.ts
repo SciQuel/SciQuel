f117ea12d5c01645545a85edbbd7ee6813ee7569
Will Saulnier <wsaulnier1004@gmail.com>
2026-01-11 22:28:51 -0500

    removed ip and spam checks

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,7 +1,6 @@
 import prisma from "@/lib/prisma";
 import { type Prisma } from "@prisma/client";
 import { NextResponse, type NextRequest } from "next/server";
-import { checkBans, checkSpam } from "../contact/tools";
 import { emailSchema } from "./schema";
 
 export async function POST(req: NextRequest) {
@@ -17,59 +16,7 @@ export async function POST(req: NextRequest) {
       { status: 400 },
     );
   }
-  const ip = req.headers.get("x-forwarded-for");
-  const isBanned = await checkBans(
-    parsedRequest.data.email,
-    ip ? ip : undefined,
-  );
-  if (isBanned) {
-    return NextResponse.json(
-      {
-        error: "Banned email or IP found",
-      },
-      { status: 403 },
-    );
-  }
-  if (isBanned === undefined) {
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 },
-    );
-  }
-  if (ip) {
-    const ipSpam = await checkSpam(false, ip);
-    if (ipSpam) {
-      return NextResponse.json(
-        {
-          error: "Banned email or IP found",
-        },
-        { status: 403 },
-      );
-    }
-    if (ipSpam === undefined) {
-      return NextResponse.json(
-        { error: "Internal Server Error" },
-        { status: 500 },
-      );
-    }
-  }
-  const emailSpam = await checkSpam(true, parsedRequest.data.email);
-  if (emailSpam) {
-    return NextResponse.json(
-      {
-        error: "Banned email or IP found",
-      },
-      { status: 403 },
-    );
-  }
-
-  if (emailSpam === undefined) {
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 },
-    );
-  }
-
+  
   try {
     const timestamp = new Date();
 
