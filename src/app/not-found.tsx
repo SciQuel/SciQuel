6932934d82bb7b53c220d9b9ef581d3c3cdfcab8
Will Saulnier <wsaulnier1004@gmail.com>
2026-01-20 01:00:45 -0500

    padding testing

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,22 +15,24 @@ export default function NotFound() {
     switch (selected) {
       case 0:
         return (
-          <div className="flex flex-col justify-center items-center h-full w-full p-40 text-black font-black">
-            <h1>Most Popular Articles</h1>
-            <button>Sample</button>
-            <button>Sample</button>
-            <button>Sample</button>
-            <span className="hover:cursor-pointer hover:underline">Explore More Articles</span>
+          <div className="flex w-full h-full justify-center items-center">
+            <div className="flex flex-col justify-center items-center text-black font-black gap-2">
+              <h1>Most Popular Articles</h1>
+              {Array.from({ length: 3 }).map((_, i) => {
+                return <div key="i" className="bg-white w-full rounded-lg">article</div>;
+              })}
+              <span className="hover:cursor-pointer hover:underline">Explore More Articles</span>
+            </div>
           </div>
         );
       case 1:
         return (
-          <div className="flex flex-col justify-center items-center h-full w-full p-40 gap-4">
-            <p>Couldn't find what you were looking for? Let us know so we can fix it!</p>
-            <form className="flex flex-col justify-center items-center gap-4">
-              <div className="bg-white rounded-lg text-black flex flex-col items-center justify-center p-4 gap-2">
+          <div className="flex flex-col justify-center items-center h-full w-full gap-4 p-36 text-center">
+            <p className="mx-8">Couldn't find what you were looking for? Let us know so we can fix it!</p>
+            <form className="flex flex-col justify-center items-center gap-4 w-full flex-1">
+              <div className="bg-white rounded-lg text-black flex flex-col items-center justify-center p-4 gap-2 w-full flex-1">
                 <span className="">Describe what you were looking for.</span>
-                <textarea className="bg-slate-300 rounded-lg w-full" style={{resize:"none"}}></textarea>
+                <textarea className="bg-slate-300 rounded-lg w-full p-4 flex-1" style={{resize:"none"}}></textarea>
               </div>
               <button className="bg-white rounded-full text-black px-4">Submit</button>
             </form>
@@ -121,7 +123,7 @@ export default function NotFound() {
         })}
       </div>
       {/* Left */}
-      <div id="not-found-left" className="flex flex-col justify-center items-center gap-10 flex-1 z-20">
+      <div id="not-found-left" className="flex flex-col justify-center items-center gap-10 flex-1 z-10">
         <span className="text-[96px]">404</span>
         <div className="flex flex-col items-center">
           <p>We can't seem to find what you're looking for.</p>
@@ -156,8 +158,8 @@ export default function NotFound() {
       </div>
 
       {/* Right */}
-      <div id="not-found-right" className="flex flex-col justify-center items-center flex-1 z-20">
-        <div className="bg-cover aspect-square overflow-hidden"
+      <div id="not-found-right" className="flex flex-col justify-center items-center flex-1 z-10">
+        <div className="bg-cover aspect-square overflow-hidden w-full"
           style={{ backgroundImage: backgroundImageArray[selected] }}
         >
           {renderContent()}
