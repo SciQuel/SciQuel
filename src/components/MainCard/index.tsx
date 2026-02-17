31ececb7901bea7e53045c126dd83acf0c96c72c
William Saulnier <124754932+wsau2@users.noreply.github.com>
2026-02-17 17:58:32 -0500

    Main card hover fix (#129)
    * main card clickable
    
    * removed redundant Link in innerCard
    
    * main card behavior fix
    
    * prettier
    
    ---------
    
    Co-authored-by: Hillary764 <hillaryhwang123@gmail.com>

--- src/components/MainCard/index.tsx
+++ src/components/MainCard/index.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { type StoryTopic, type StoryType } from "@prisma/client";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 import InnerCard from "./InnerCard";
 
 interface Props {
@@ -24,16 +27,23 @@ export default function MainCard({
   thumbnailUrl,
   href,
 }: Props) {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <div
-      className={`relative z-0 mx-auto 
-                  pt-12 text-center
-                  transition-all duration-300
-                   hover:scale-[1.03] xs:min-w-[300px] md:h-[70vh] md:pt-0  xl:min-h-[300px]`}
+      className={`
+        relative z-0 mx-auto pt-12 text-center
+        transition-all duration-300
+        ${hovered ? "scale-[1.03]" : ""}
+        xs:min-w-[300px] md:h-[70vh] md:pt-0 xl:min-h-[300px]
+      `}
     >
-      <div
+      <Link
+        href={href}
         className={`absolute right-0 top-0 z-10 h-full 
                     w-full justify-center bg-cover bg-center transition-all duration-300 lg:w-8/12`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
         <Image
           src={thumbnailUrl}
@@ -42,17 +52,22 @@ export default function MainCard({
           className="rounded-md"
           style={{ objectFit: "cover" }}
         />
-      </div>
+      </Link>
       <div className="relative flex h-full w-full flex-col justify-end p-5 transition-all duration-300 lg:w-3/5 lg:justify-center">
-        <InnerCard
-          href={href}
-          title={title}
-          subtitle={subtitle}
-          author={author}
-          date={date}
-          mediaType={mediaType}
-          tag={tag}
-        />
+        <div
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <InnerCard
+            href={href}
+            title={title}
+            subtitle={subtitle}
+            author={author}
+            date={date}
+            mediaType={mediaType}
+            tag={tag}
+          />
+        </div>
         {/* <div className="z-10 mx-5 my-3 hidden text-left transition-all duration-300 lg:relative lg:top-16 lg:block">
           <p className="font-sourceSerif4 text-xl font-[350] text-sciquelTeal">
             {author} | {date}
